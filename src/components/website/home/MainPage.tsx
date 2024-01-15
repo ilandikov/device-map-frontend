f3daed974ebb46fe4bf31fbc056173c90903eb08
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-15 20:55:25 +0600

    feat: - set background color to black

--- src/components/website/home/MainPage.tsx
+++ src/components/website/home/MainPage.tsx
@@ -1,11 +1,13 @@
 /* External dependencies */
 import React from 'react';
+import Header from '../../header/header';
 
 /* Local dependencies */
 
 export default function MainPage() {
   return (
     <>
+      <Header />
     </>
   );
 }
