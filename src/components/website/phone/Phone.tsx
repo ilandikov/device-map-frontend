6363c446de2c309139ed8974d263d409577fca76
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-16 17:34:39 +0600

    refactor: - extract textBlock component

--- src/components/website/phone/Phone.tsx
+++ src/components/website/phone/Phone.tsx
@@ -3,21 +3,20 @@ import React, { CSSProperties } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import './phone.css';
 
+function textBlock(header: string) {
+    return (
+        <div className="phone-header-and-description">
+            <p className='header'>{header}</p>
+            <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
+        </div>
+    );
+}
 export default function Phone() {
     return (
         <div className="phone-background phone-text-blocks">
-            <div className="phone-header-and-description">
-                <p className='header'>Исследуйте</p>
-                <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
-            </div>
-            <div className="phone-header-and-description">
-                <p className='header'>Отмечайте</p>
-                <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
-            </div>
-            <div className="phone-header-and-description">
-                <p className='header'>Зарабатывай баллы</p>
-                <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
-            </div>
+            {textBlock("Исследуйте")}
+            {textBlock("Отмечайте")}
+            {textBlock("Зарабатывай баллы")}
         </div>
     );
 }
