265a06b1d0eaee3a15f663fbab761dace66090fe
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-16 17:51:13 +0600

    feat: - add frame image & text

--- src/components/website/map/Map.tsx
+++ src/components/website/map/Map.tsx
@@ -1,11 +1,17 @@
 /* External dependencies */
 import React, { CSSProperties } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 /* Local dependencies */
 import './map.css';
 
 export default function Map() {
     return (
-        <div className="map-background"></div>
+        <div className="map-background">
+            <div className="frame-column">
+                <StaticImage src="./Frame.png" alt="frame" />
+                <p className="frame-text">Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
+            </div>
+        </div>
     );
 }
