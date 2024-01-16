f8e1b27b088b8a91e2b3dbf07e5bec7a17266396
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-16 16:10:12 +0600

    feat: - style Phone headers and descriptions

--- src/components/website/phone/Phone.tsx
+++ src/components/website/phone/Phone.tsx
@@ -6,15 +6,15 @@ import './phone.css';
 export default function Phone() {
     return (
         <div className="phone-background phone-text-blocks">
-            <div>
+            <div className="phone-header-and-description">
                 <p className='header'>Исслейдуте</p>
                 <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
             </div>
-            <div>
+            <div className="phone-header-and-description">
                 <p className='header'>Отмечайте</p>
                 <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
             </div>
-            <div>
+            <div className="phone-header-and-description">
                 <p className='header'>Зарабатывай баллы</p>
                 <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
             </div>
