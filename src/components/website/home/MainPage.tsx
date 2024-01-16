ba997b88ad16ca8954065ae68d9eba753213cee0
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-16 15:13:13 +0600

    feat: - add site-container to align by the center

--- src/components/website/home/MainPage.tsx
+++ src/components/website/home/MainPage.tsx
@@ -1,13 +1,17 @@
 /* External dependencies */
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import Header from '../../header/header';
 
 /* Local dependencies */
 
 export default function MainPage() {
+  const siteContainer: CSSProperties = {
+    maxWidth: '1200px',
+    margin: '0 auto',
+  }
   return (
-    <>
+    <div style={siteContainer}>
       <Header />
-    </>
+    </div>
   );
 }
