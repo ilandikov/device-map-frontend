c6a0c6b978cb63d4e4b83dd95f076bcb6bb04ec3
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-17 11:40:25 +0600

    feat: - populate the first advantage block

--- src/components/website/advantages/advantages.tsx
+++ src/components/website/advantages/advantages.tsx
@@ -7,7 +7,11 @@ export default function Advantages() {
     return (
         <div className='advantages-container'>
             <div className='advantage-block-full'>
-                
+                <StaticImage src="./iPhone 14.png" alt="logo" />
+                <div className='advantage-block-text'>
+                    <p className='advantage-block-text-header'>Играй.</p>
+                    <p>Увлекательная игра</p>
+                </div>
             </div>
         </div>
       );
