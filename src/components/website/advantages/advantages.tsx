ae2970e9e9d0589037b2883a23ce6f6ca86956ad
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-17 12:37:31 +0600

    refactor: - add empty AdvantageHalfBlock()

--- src/components/website/advantages/advantages.tsx
+++ src/components/website/advantages/advantages.tsx
@@ -19,10 +19,18 @@ function AdvantageFullBlock(header: string, description: string, textColor: stri
         </div>
     );
 }
+
+function AdvantageHalfBlock() {
+    return(
+        <div className='advantage-block-half'>
+        </div>
+    );
+}
 export default function Advantages() {
     return (
         <div className='advantages-container'>
             {AdvantageFullBlock('Играй','Увлекательная игра', '#000000', '#CBEA5E', <StaticImage src="./iPhone 14.png" alt="advantage" />)}
+            {AdvantageHalfBlock()}
             {AdvantageFullBlock('Везде','На всех платформах', '#FFFFFF', '#0066FF', <StaticImage src="./MacBook Pro 16.png" alt="advantage" />)}
             {AdvantageFullBlock('Оглянись','Терминалы везде', '#000000', '#FFFFFF', <StaticImage src="./Terminal.png" alt="advantage" />)}
         </div>
