72411ab144b71a13e13930d6df1145f362ec7b05
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-17 19:32:10 +0600

    refactor: ! remove unused Footer CSS classes

--- src/components/website/footer/Footer.tsx
+++ src/components/website/footer/Footer.tsx
@@ -13,13 +13,13 @@ export default function Footer() {
             <p className='footer-brand-bigger-text'>Удобное и надежное отечественное приложение для оплаты в Кыргызстане</p>
             <p className='footer-brand-smaller-text'>©2022 Averspay Лицензия НБКР № 3006010615, № 2006010615 от 06.02.2015</p>
           </div>
-          <div className='footer-products footer-links'>
+          <div className='footer-links'>
             <p className='footer-first-link'>Продукты</p>
             <p>Кошелек</p>
             <p>Терминалы</p>
             <p>Эквайринг</p>
           </div>
-          <div className='footer-about footer-links'>
+          <div className='footer-links'>
             <p className='footer-first-link'>О нас</p>
             <p>Компания</p>
             <p>Документы</p>
