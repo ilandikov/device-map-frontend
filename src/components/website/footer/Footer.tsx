59f97fe95c5afd37d84c24e2b8695b28820ec0e0
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-17 17:31:14 +0600

    feat: - add brand information to the Footer

--- src/components/website/footer/Footer.tsx
+++ src/components/website/footer/Footer.tsx
@@ -1,9 +1,24 @@
 /* External dependencies */
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
+
+/* Local dependencies */
+import './footer.css';
 
 export default function Footer() {
     return (
         <div className='footer-container'>
+          <div className='footer-brand'>
+            <StaticImage src="./LogoWithBrand.png" alt="logo" />
+            <p className='footer-brand-bigger-text'>Удобное и надежное отечественное приложение для оплаты в Кыргызстане</p>
+            <p className='footer-brand-smaller-text'>©2022 Averspay Лицензия НБКР № 3006010615, № 2006010615 от 06.02.2015</p>
+          </div>
+          <div className='footer-products'>
+          </div>
+          <div className='footer-about'>
+          </div>
+          <div className='footer-contacts'>
+          </div>
         </div>
       );
 }
