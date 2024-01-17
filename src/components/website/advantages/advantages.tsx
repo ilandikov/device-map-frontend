364c7ed191367a67bb1694370a7b70ff02d217cc
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-17 14:29:36 +0600

    feat: - set the width of the half block text

--- src/components/website/advantages/advantages.tsx
+++ src/components/website/advantages/advantages.tsx
@@ -33,7 +33,7 @@ function AdvantageHalfBlock() {
     return(
         <div className='advantage-block'>
             <div className='advantage-half-block advantage-block-radius' style={textBlockCSS}>
-                <div className='advantage-text advantage-text-align-center'>
+                <div className='advantage-text advantage-half-block-text'>
                     <p className='advantage-text-header'>Обменивай.</p>
                     <p>Получай подарки</p>
                 </div>
