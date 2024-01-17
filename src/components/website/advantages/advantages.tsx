55f9a442b5af819da5451127e30ce87e7a4547ef
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-17 11:42:59 +0600

    refactor: - extract AdvantageFullBlock() component

--- src/components/website/advantages/advantages.tsx
+++ src/components/website/advantages/advantages.tsx
@@ -3,16 +3,21 @@ import React, { CSSProperties } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import './advantages.css';
 
+function AdvantageFullBlock() {
+    return(
+        <div className='advantage-block-full'>
+            <StaticImage src="./iPhone 14.png" alt="logo" />
+            <div className='advantage-block-text'>
+                <p className='advantage-block-text-header'>Играй.</p>
+                <p>Увлекательная игра</p>
+            </div>
+        </div>
+    );
+}
 export default function Advantages() {
     return (
         <div className='advantages-container'>
-            <div className='advantage-block-full'>
-                <StaticImage src="./iPhone 14.png" alt="logo" />
-                <div className='advantage-block-text'>
-                    <p className='advantage-block-text-header'>Играй.</p>
-                    <p>Увлекательная игра</p>
-                </div>
-            </div>
+            {AdvantageFullBlock()}
         </div>
       );
 }
