e22de8cd862b69b8c531d6483177e9e412c91eec
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-18 17:56:33 +0600

    refactor: - rename CSS classes in Map

--- src/components/website/map/Map.tsx
+++ src/components/website/map/Map.tsx
@@ -8,12 +8,12 @@ import './map.css';
 export default function Map() {
     return (
         <section className="map-background">
-            <div className="frame-column">
+            <div className="map-content-container">
                 <div className='map-brand'>
                     <StaticImage src="../../../assets/images/Logo.svg" alt="logo-with-brand" />
                     <p className='map-brand-text'>Карта</p>
                 </div>
-                <p className="frame-text">Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
+                <p className="map-text">Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
                 <StaticImage src="../../../assets/images/Links.png" alt="links" />
             </div>
         </section>
