34ad17882fa7a840a4a4254d11bce851a6e667b4
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-18 17:58:18 +0600

    refactor: ! move textBlocks()

--- src/components/website/phone/Phone.tsx
+++ src/components/website/phone/Phone.tsx
@@ -2,14 +2,6 @@
 import React, { CSSProperties } from 'react';
 import './phone.css';
 
-function textBlock(header: string) {
-    return (
-        <div className="phone-header-and-description">
-            <p className='header'>{header}</p>
-            <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
-        </div>
-    );
-}
 export default function Phone() {
     const headers = [
         "Исследуйте",
@@ -25,5 +17,14 @@ export default function Phone() {
         <section className="phone-background phone-text-blocks">
             {textBlocks}
         </section>
+    );
+}
+
+function textBlock(header: string) {
+    return (
+        <div className="phone-header-and-description">
+            <p className='header'>{header}</p>
+            <p className='description'>Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
+        </div>
     );
 }
