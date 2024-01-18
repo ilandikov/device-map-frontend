3f39cd44167cc7532dcbb45159c0ab5cd3df45a0
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-18 11:20:44 +0600

    refactor: - replace Frame image with Logo and brand name

--- src/components/website/map/Map.tsx
+++ src/components/website/map/Map.tsx
@@ -9,7 +9,10 @@ export default function Map() {
     return (
         <div className="map-background">
             <div className="frame-column">
-                <StaticImage src="./Frame.png" alt="frame" />
+                <div className='map-brand'>
+                    <StaticImage src="./../logo/Logo.png" alt="logo-with-brand" />
+                    <p className='map-brand-text'>Карта</p>
+                </div>
                 <p className="frame-text">Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
                 <StaticImage src="./Links.png" alt="links" />
             </div>
