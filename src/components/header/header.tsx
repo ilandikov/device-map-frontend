eab4c13937c80baaca9fe8dd3e8aabb95d2b7318
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-18 17:51:27 +0600

    refactor: ! capitalise Header()

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.scss'
 import Logo from '../website/logo/Logo'
 
-export default function header() {
+export default function Header() {
   return (
     <section>
       <Logo />
