d1a12153d1cce2cb90e16d7ee7ea879ced17b6cd
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-18 11:28:36 +0600

    refactor: ! replace logo with vector image

--- src/components/website/map/Map.tsx
+++ src/components/website/map/Map.tsx
@@ -10,7 +10,7 @@ export default function Map() {
         <div className="map-background">
             <div className="frame-column">
                 <div className='map-brand'>
-                    <StaticImage src="./../logo/Logo.png" alt="logo-with-brand" />
+                    <StaticImage src="./Logo.svg" alt="logo-with-brand" />
                     <p className='map-brand-text'>Карта</p>
                 </div>
                 <p className="frame-text">Карта терминалов от Finik, исследуйте, отмечайте и зарабатывайте баллы</p>
