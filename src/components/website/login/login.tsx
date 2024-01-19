8e70e09b9431d0852edef0bc12320d04b470c11d
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-19 16:00:04 +0600

    feat: ! add background image to Login

--- src/components/website/login/login.tsx
+++ src/components/website/login/login.tsx
@@ -1,9 +1,14 @@
 /* External dependencies */
 import React from "react";
 
+/* Local dependencies */
+import './login.css';
+
 export default function Login() {
     return (
-        <>
-        </>
+        <div className='login-container'>
+          <div className='login-background'>
+          </div>
+        </div>
       );
 }
