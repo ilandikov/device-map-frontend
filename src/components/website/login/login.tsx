868966984877ee8733390250b6ebc56a59b971aa
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-19 16:09:48 +0600

    feat: ! add login window at the center

--- src/components/website/login/login.tsx
+++ src/components/website/login/login.tsx
@@ -8,6 +8,8 @@ export default function Login() {
     return (
         <div className='login-container'>
           <div className='login-background'>
+            <div className='login-window'>
+            </div>
           </div>
         </div>
       );
