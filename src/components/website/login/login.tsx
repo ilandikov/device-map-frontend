8d6066f937ea45c2256b711a6a31b0f30426fbcd
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-21 03:24:49 +0600

    refactor: ! extract Ellipses and LoginWelcolme components locally

--- src/components/website/login/login.tsx
+++ src/components/website/login/login.tsx
@@ -57,33 +57,47 @@ export default function Login() {
             {pins}
           </div>
           <div className='login-window'>
-            <div className='login-ellipses-container'>
-              <div className='login-ellipse-left-container'>
-                <div className='login-ellipse login-ellipse-green'>
-                </div>
-              </div>
-              <div className='login-ellipse-right-container'>
-                <div className='login-ellipse login-ellipse-blue'>
-                </div>
-              </div>
-            </div>
+            <Ellipses />
             <div className='login-window-content-container'>
-              <div className='login-window-top-container'>
-                <StaticImage className='login-window-logo' src='../../../assets/images/LogoGreen.svg' alt='logo' />
-                <p className='login-window-brand'>Финик Карта</p>
-                <p className='login-window-description'>Отмечай места на карте где нет нашего терминала, мы поставим его, а тебе пришлем бонусы которые ты сможешь обменять на реальные призы</p>
-              </div>
-              <div className='login-window-bottom-container'>
-                <div className='login-window-button login-window-button-signin'>
-                  <p>Войти в аккаунт</p>
-                </div>
-                <div className='login-window-button login-window-button-register'>
-                  <p>Регистрация</p>
-                </div>
-              </div>
+              <LoginWelcome />
             </div>
           </div>
         </div>
       </div>
     );
+}
+
+function Ellipses() {
+  return(
+    <div className='login-ellipses-container'>
+      <div className='login-ellipse-left-container'>
+        <div className='login-ellipse login-ellipse-green'>
+        </div>
+      </div>
+      <div className='login-ellipse-right-container'>
+        <div className='login-ellipse login-ellipse-blue'>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function LoginWelcome() {
+  return(
+    <>
+      <div className='login-window-top-container'>
+        <StaticImage className='login-window-logo' src='../../../assets/images/LogoGreen.svg' alt='logo' />
+        <p className='login-window-brand'>Финик Карта</p>
+        <p className='login-window-description'>Отмечай места на карте где нет нашего терминала, мы поставим его, а тебе пришлем бонусы которые ты сможешь обменять на реальные призы</p>
+      </div>
+      <div className='login-window-bottom-container'>
+        <div className='login-window-button login-window-button-signin'>
+          <p>Войти в аккаунт</p>
+        </div>
+        <div className='login-window-button login-window-button-register'>
+          <p>Регистрация</p>
+        </div>
+      </div>
+    </>
+  );
 }
