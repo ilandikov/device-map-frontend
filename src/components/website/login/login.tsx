dfcc4d8a93b0fbc03ba6c3d2bd3a4b2a5a92ca33
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-22 16:08:56 +0600

    refactor: ! add empty element in Login return

--- src/components/website/login/login.tsx
+++ src/components/website/login/login.tsx
@@ -7,31 +7,33 @@ import { StaticImage } from "gatsby-plugin-image";
 
 export default function Login() {
   return (
-      <div className='login-container'>
-        <Pins />
-        <div className='login-header'>
-          <div className='login-header-block'>
-              <StaticImage className='login-header-brand-logo' src='../../../assets/images/LogoGreen.svg' alt='logo' />
-              <p className='login-header-brand-text'>Карта</p>
-          </div>
-          <div className='login-header-block'>
-            <div className='login-header-inner-block'>
-              <StaticImage className='login-header-account-image' src="../../../assets/images/Account.svg" alt="login-header-account" />
-              <p className='login-header-account-text'>Войти</p>
+      <>
+        <div className='login-container'>
+          <Pins />
+          <div className='login-header'>
+            <div className='login-header-block'>
+                <StaticImage className='login-header-brand-logo' src='../../../assets/images/LogoGreen.svg' alt='logo' />
+                <p className='login-header-brand-text'>Карта</p>
             </div>
-            <div className='login-header-inner-block'>
-              <StaticImage className='login-header-apps-google-play' src="../../../assets/images/GooglePlay.svg" alt="login-header-apps-google-play" />
-              <StaticImage className='login-header-apps-app-store'src="../../../assets/images/AppStore.svg" alt="login-header-apps-app-store" />
+            <div className='login-header-block'>
+              <div className='login-header-inner-block'>
+                <StaticImage className='login-header-account-image' src="../../../assets/images/Account.svg" alt="login-header-account" />
+                <p className='login-header-account-text'>Войти</p>
+              </div>
+              <div className='login-header-inner-block'>
+                <StaticImage className='login-header-apps-google-play' src="../../../assets/images/GooglePlay.svg" alt="login-header-apps-google-play" />
+                <StaticImage className='login-header-apps-app-store'src="../../../assets/images/AppStore.svg" alt="login-header-apps-app-store" />
+              </div>
             </div>
           </div>
+          <div className='login-window'>
+              <Ellipses />
+              <div className='login-window-content-container'>
+                <LoginWelcome />
+              </div>
+            </div>
         </div>
-        <div className='login-window'>
-            <Ellipses />
-            <div className='login-window-content-container'>
-              <LoginWelcome />
-            </div>
-          </div>
-      </div>
+      </>
     );
 }
 
