cf874d4e81e70f2bb743695377956aaac082edc9
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-26 13:15:09 +0600

    refactor: extract common footer-brands-opaque-text class

--- src/components/website/footer/Footer.tsx
+++ src/components/website/footer/Footer.tsx
@@ -14,10 +14,10 @@ export default function Footer() {
                 <StaticImage src="../../../assets/images/Logo.svg" alt="logo" />
                 <p className='footer-brand-name'>Finik</p>
               </div>
-              <p className='footer-brand-bigger-text'>Удобное и надежное отечественное приложение для оплаты в Кыргызстане</p>
+              <p className='footer-brand-opaque-text footer-brand-bigger-text'>Удобное и надежное отечественное приложение для оплаты в Кыргызстане</p>
               <div>
-                <p className='footer-brand-smaller-text'>©2022 Averspay</p>
-                <p className='footer-brand-smaller-text'>Лицензия НБКР № 3006010615, № 2006010615 от 06.02.2015</p>
+                <p className='footer-brand-opaque-text footer-brand-smaller-text'>©2022 Averspay</p>
+                <p className='footer-brand-opaque-text footer-brand-smaller-text'>Лицензия НБКР № 3006010615, № 2006010615 от 06.02.2015</p>
               </div>
             </div>
             <div className='footer-links'>
