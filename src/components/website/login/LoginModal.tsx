ee0e40d5f15f9069187b398596d2c5c63a0b89cc
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-01-30 14:14:09 +0600

    refactor: - rename window to modal in CSS classes

--- src/components/website/login/LoginModal.tsx
+++ src/components/website/login/LoginModal.tsx
@@ -5,26 +5,26 @@ import { StaticImage } from 'gatsby-plugin-image';
 export function LoginModal() {
     const { t } = useI18next();
     return (
-        <div className="login-window-container">
-            <div className="login-window">
+        <div className="login-modal-container">
+            <div className="login-modal">
                 <Ellipses />
-                <div className="login-window-content-container">
-                    <div className="login-window-top-container">
+                <div className="login-modal-content-container">
+                    <div className="login-modal-top-container">
                         <StaticImage
-                            className="login-window-logo"
+                            className="login-modal-logo"
                             src="../../../assets/images/LogoGreen.svg"
                             alt="logo"
                         />
-                        <p className="login-window-brand">
+                        <p className="login-modal-brand">
                             {t('brand')} {t('map')}
                         </p>
-                        <p className="login-window-description">{t('loginCallToAction')}</p>
+                        <p className="login-modal-description">{t('loginCallToAction')}</p>
                     </div>
-                    <div className="login-window-bottom-container">
-                        <div className="login-window-button login-window-button-sign-in">
+                    <div className="login-modal-bottom-container">
+                        <div className="login-modal-button login-modal-button-sign-in">
                             <p>{t('accountLogin')}</p>
                         </div>
-                        <div className="login-window-button login-window-button-register">
+                        <div className="login-modal-button login-modal-button-register">
                             <p>{t('accountRegister')}</p>
                         </div>
                     </div>
