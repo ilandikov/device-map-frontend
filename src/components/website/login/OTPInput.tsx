618166fbb564cddc246775b739b0e474704158d5
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-09 16:26:56 +0600

    refactor: . extract inputElements variable

--- src/components/website/login/OTPInput.tsx
+++ src/components/website/login/OTPInput.tsx
@@ -3,20 +3,15 @@ import React from 'react';
 
 export function OTPInput() {
     const { t } = useI18next();
+
+    const inputElements = [0, 1, 2, 3, 4, 5].map((index) => (
+        <input type="number" pattern="[0-9]" maxLength={1} key={`OTPInput${index}`} data-testid={`OTPInput${index}`} />
+    ));
+
     return (
         <div className="login-modal-input-outer-container">
             <p className="login-modal-input-description">{t('OTPEnter')}</p>
-            <div className="login-modal-input-otp-container">
-                {[0, 1, 2, 3, 4, 5].map((index) => (
-                    <input
-                        type="number"
-                        pattern="[0-9]"
-                        maxLength={1}
-                        key={`OTPInput${index}`}
-                        data-testid={`OTPInput${index}`}
-                    />
-                ))}
-            </div>
+            <div className="login-modal-input-otp-container">{inputElements}</div>
             <p className="login-modal-input-description login-modal-opaque-text">{t('OTPExplanation')}</p>
             <p className="login-modal-input-description login-modal-correct-input">{t('OTPSendAgain')}</p>
         </div>
