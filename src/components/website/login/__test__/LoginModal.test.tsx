9d40bacf0ecd06e9980a10ac78bd0e49a6456883
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-09 23:39:31 +0600

    test: . create UserAuthStateUtils.test.ts with tests for UserAuthStateUtils.ts tests

--- src/components/website/login/__test__/LoginModal.test.tsx
+++ src/components/website/login/__test__/LoginModal.test.tsx
@@ -9,7 +9,6 @@ import '@testing-library/jest-dom';
 import { configureTestStore } from '../../../../../tests/utils';
 import { LoginModal, UserAuthState } from '../LoginModal';
 import * as userAuthStateUtils from '../UserAuthStateUtils';
-import { userAuthStateFromUserEmail, userAuthStateFromUserPasswords } from '../UserAuthStateUtils';
 
 jest.mock('gatsby-plugin-react-i18next', () => ({
     ...jest.requireActual('gatsby-plugin-react-i18next'),
@@ -302,51 +301,5 @@ describe('LoginModal action tests', () => {
         fireEvent.change(OTPInput0, { target: { value: '1' } });
 
         expect(OTPInput3).toHaveFocus();
-    });
-});
-
-describe('user email logic tests', () => {
-    it('should move to password creation when new email is presented', () => {
-        const email = 'good@email.com';
-
-        const newUserState = userAuthStateFromUserEmail(email);
-
-        expect(newUserState).toEqual(UserAuthState.PASSWORD_CREATION);
-    });
-
-    it('should move to email not valid stage when a bad email is presented', () => {
-        const email = 'this is not an email!';
-
-        const newUserState = userAuthStateFromUserEmail(email);
-
-        expect(newUserState).toEqual(UserAuthState.MAIL_NOT_VALID);
-    });
-
-    it('should move to email already exists stage when already existing mail is presented', () => {
-        const email = 'already@exists.com';
-
-        const newUserState = userAuthStateFromUserEmail(email);
-
-        expect(newUserState).toEqual(UserAuthState.MAIL_ALREADY_EXISTS);
-    });
-});
-
-describe('user password logic tests', () => {
-    it('should move to OTP input if passwords match', () => {
-        const nextUserAuthState = userAuthStateFromUserPasswords('passwordsMatch', 'passwordsMatch');
-
-        expect(nextUserAuthState).toEqual(UserAuthState.OTP_INPUT);
-    });
-
-    it('should move to error state if passwords dont match', () => {
-        const nextUserAuthState = userAuthStateFromUserPasswords('passwordsDontMatch', 'passwordsMatch');
-
-        expect(nextUserAuthState).toEqual(UserAuthState.PASSWORD_CREATION_MATCH_ERROR);
-    });
-
-    it.failing('should move to error state if password has not been input or it is an empty string', () => {
-        const nextUserAuthState = userAuthStateFromUserPasswords('', '');
-
-        expect(nextUserAuthState).toHaveBeenCalledWith(UserAuthState.PASSWORD_CREATION_MATCH_ERROR);
     });
 });
---
+++ src/components/website/login/__test__/UserAuthStateUtils.test.ts
@@ -0,0 +1,48 @@
+import { userAuthStateFromUserEmail, userAuthStateFromUserPasswords } from '../UserAuthStateUtils';
+import { UserAuthState } from '../LoginModal';
+
+describe('user email logic tests', () => {
+    it('should move to password creation when new email is presented', () => {
+        const email = 'good@email.com';
+
+        const newUserState = userAuthStateFromUserEmail(email);
+
+        expect(newUserState).toEqual(UserAuthState.PASSWORD_CREATION);
+    });
+
+    it('should move to email not valid stage when a bad email is presented', () => {
+        const email = 'this is not an email!';
+
+        const newUserState = userAuthStateFromUserEmail(email);
+
+        expect(newUserState).toEqual(UserAuthState.MAIL_NOT_VALID);
+    });
+
+    it('should move to email already exists stage when already existing mail is presented', () => {
+        const email = 'already@exists.com';
+
+        const newUserState = userAuthStateFromUserEmail(email);
+
+        expect(newUserState).toEqual(UserAuthState.MAIL_ALREADY_EXISTS);
+    });
+});
+
+describe('user password logic tests', () => {
+    it('should move to OTP input if passwords match', () => {
+        const nextUserAuthState = userAuthStateFromUserPasswords('passwordsMatch', 'passwordsMatch');
+
+        expect(nextUserAuthState).toEqual(UserAuthState.OTP_INPUT);
+    });
+
+    it('should move to error state if passwords dont match', () => {
+        const nextUserAuthState = userAuthStateFromUserPasswords('passwordsDontMatch', 'passwordsMatch');
+
+        expect(nextUserAuthState).toEqual(UserAuthState.PASSWORD_CREATION_MATCH_ERROR);
+    });
+
+    it.failing('should move to error state if password has not been input or it is an empty string', () => {
+        const nextUserAuthState = userAuthStateFromUserPasswords('', '');
+
+        expect(nextUserAuthState).toHaveBeenCalledWith(UserAuthState.PASSWORD_CREATION_MATCH_ERROR);
+    });
+});
