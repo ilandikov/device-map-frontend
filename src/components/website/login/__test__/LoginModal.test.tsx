c3d28e0a13bcc1ec91ec0b6061826cef6ed45740
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-12 16:45:27 +0600

    test: - move OTP input tests to a separate file and mock the next button reference

--- src/components/website/login/__test__/LoginModal.test.tsx
+++ src/components/website/login/__test__/LoginModal.test.tsx
@@ -231,6 +231,7 @@ describe('LoginModal action tests - password creation stages', () => {
     });
 });
 
+// TODO after OTP button & input will be united in one component, this shall go to the tests of that component.
 describe('LoginModal action tests - OTP stages', () => {
     it('should transition to loading from OTP stage', () => {
         mockUseUserAuthState(UserAuthState.OTP_INPUT);
@@ -242,93 +243,5 @@ describe('LoginModal action tests - OTP stages', () => {
         fireEvent.click(nextButton);
 
         expect(setUserAuthState).toHaveBeenCalledWith(UserAuthState.OTP_LOADING);
-    });
-});
-
-describe('OTP input tests', () => {
-    it.each([0, 1, 2, 3, 4, 5])('should enter numeric characters in OTP input number %i', (inputIndex) => {
-        mockUseUserAuthState(UserAuthState.OTP_INPUT);
-        const { container } = render(componentWithStoreProvider);
-        const OTPInput = getByTestId(container, `OTPInput${inputIndex}`) as HTMLInputElement;
-
-        expect(OTPInput).toBeInTheDocument();
-        fireEvent.change(OTPInput, { target: { value: `${inputIndex}` } });
-
-        expect(OTPInput.value).toEqual(`${inputIndex}`);
-    });
-
-    it.each([0, 1, 2, 3, 4, 5])('should not enter non numeric characters in OTP input number %i', (inputIndex) => {
-        mockUseUserAuthState(UserAuthState.OTP_INPUT);
-        const { container } = render(componentWithStoreProvider);
-        const OTPInput = getByTestId(container, `OTPInput${inputIndex}`) as HTMLInputElement;
-
-        expect(OTPInput).toBeInTheDocument();
-        fireEvent.change(OTPInput, { target: { value: 'a' } });
-
-        expect(OTPInput.value).toEqual('');
-    });
-
-    it.each([0, 1, 2, 3, 4])(
-        'should focus on next input element when a digit is input for input %i (Only the first 5 inputs, index=0...4)',
-        (inputIndex) => {
-            mockUseUserAuthState(UserAuthState.OTP_INPUT);
-            const { container } = render(componentWithStoreProvider);
-            const OTPInput = getByTestId(container, `OTPInput${inputIndex}`) as HTMLInputElement;
-
-            expect(OTPInput).toBeInTheDocument();
-            OTPInput.focus();
-            fireEvent.change(OTPInput, { target: { value: '1' } });
-
-            const nextOTPInput = getByTestId(container, `OTPInput${inputIndex + 1}`) as HTMLInputElement;
-            expect(nextOTPInput).toBeInTheDocument();
-            expect(nextOTPInput).toHaveFocus();
-        },
-    );
-
-    it('should focus on "next" button when a digit is input for last input (index = 5)', () => {
-        mockUseUserAuthState(UserAuthState.OTP_INPUT);
-        const { container } = render(componentWithStoreProvider);
-        const OTPInput = getByTestId(container, 'OTPInput5') as HTMLInputElement;
-
-        expect(OTPInput).toBeInTheDocument();
-        OTPInput.focus();
-        fireEvent.change(OTPInput, { target: { value: '1' } });
-
-        const nextButton = getByText(container, 'next');
-
-        expect(nextButton).toBeInTheDocument();
-        expect(nextButton).toHaveFocus();
-    });
-
-    it.each([0, 1, 2, 3, 4, 5])(
-        'should rewrite an existing value that has already been input in OTP input number %i',
-        (inputIndex) => {
-            mockUseUserAuthState(UserAuthState.OTP_INPUT);
-            const { container } = render(componentWithStoreProvider);
-            const OTPInput = getByTestId(container, `OTPInput${inputIndex}`) as HTMLInputElement;
-
-            expect(OTPInput).toBeInTheDocument();
-            fireEvent.change(OTPInput, { target: { value: '3' } });
-            expect(OTPInput.value).toEqual('3');
-
-            OTPInput.focus();
-            expect(OTPInput.value).toEqual('');
-        },
-    );
-
-    it('should focus on the next empty input after a digit has been input', () => {
-        mockUseUserAuthState(UserAuthState.OTP_INPUT);
-        const { container } = render(componentWithStoreProvider);
-        const OTPInput0 = getByTestId(container, 'OTPInput0') as HTMLInputElement;
-        const OTPInput1 = getByTestId(container, 'OTPInput1') as HTMLInputElement;
-        const OTPInput2 = getByTestId(container, 'OTPInput2') as HTMLInputElement;
-        const OTPInput3 = getByTestId(container, 'OTPInput3') as HTMLInputElement;
-        fireEvent.change(OTPInput1, { target: { value: '1' } });
-        fireEvent.change(OTPInput2, { target: { value: '2' } });
-
-        OTPInput0.focus();
-        fireEvent.change(OTPInput0, { target: { value: '1' } });
-
-        expect(OTPInput3).toHaveFocus();
     });
 });
