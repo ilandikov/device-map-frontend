0e318c9b00cf9cbed1d2e4d7c2cb8d2541b5c9ae
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-13 00:44:35 +0600

    refactor: - move LoginModal() CSS classes to a separate file

--- src/components/website/login/LoginModalHeader.tsx
+++ src/components/website/login/LoginModalHeader.tsx
@@ -3,12 +3,12 @@ import LogoGreen from '/src/assets/images/LogoGreen.svg';
 
 export function LoginModalHeader(props: { header: string; description: string; opaqueDescription: boolean }) {
     return (
-        <>
+        <div className="login-modal-header-container">
             <img className="login-modal-logo" src={LogoGreen} alt="login-modal-logo" />
             <p className="login-modal-header">{props.header}</p>
             <p className={`login-modal-header-description${props.opaqueDescription ? ' login-modal-opaque-text' : ''}`}>
                 {props.description}
             </p>
-        </>
+        </div>
     );
 }
