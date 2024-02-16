3168fcf567156a53f2a750303b03ddb5571e240d
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-16 17:07:13 +0600

    test: . inline mockLoginModalHooks()

--- src/components/website/login/__test__/LoginModalTestHelpers.ts
+++ src/components/website/login/__test__/LoginModalTestHelpers.ts
@@ -3,17 +3,13 @@ export let setUserEmail: jest.Mock;
 export let setUserPassword: jest.Mock;
 export let setUserPasswordRepeat: jest.Mock;
 
-export function mockLoginModalHooks() {
-    return {
+export function resetHookMocks() {
+    const hooks = {
         setUserAuthState: jest.fn().mockImplementation((userAuthState) => userAuthState),
         setUserEmail: jest.fn().mockImplementation((userEmail) => userEmail),
         setUserPassword: jest.fn().mockImplementation((userEmail) => userEmail),
         setUserPasswordRepeat: jest.fn().mockImplementation((userEmail) => userEmail),
     };
-}
-
-export function resetHookMocks() {
-    const hooks = mockLoginModalHooks();
     setUserAuthState = hooks.setUserAuthState;
     setUserEmail = hooks.setUserEmail;
     setUserPassword = hooks.setUserPassword;
