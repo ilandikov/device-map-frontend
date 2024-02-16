efc07f2cbb8b838537fe44c2e9b2b161e422dcd3
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-16 17:08:15 +0600

    test: ! hooks variable

--- src/components/website/login/__test__/LoginModalTestHelpers.ts
+++ src/components/website/login/__test__/LoginModalTestHelpers.ts
@@ -4,14 +4,8 @@ export let setUserPassword: jest.Mock;
 export let setUserPasswordRepeat: jest.Mock;
 
 export function resetHookMocks() {
-    const hooks = {
-        setUserAuthState: jest.fn().mockImplementation((userAuthState) => userAuthState),
-        setUserEmail: jest.fn().mockImplementation((userEmail) => userEmail),
-        setUserPassword: jest.fn().mockImplementation((userEmail) => userEmail),
-        setUserPasswordRepeat: jest.fn().mockImplementation((userEmail) => userEmail),
-    };
-    setUserAuthState = hooks.setUserAuthState;
-    setUserEmail = hooks.setUserEmail;
-    setUserPassword = hooks.setUserPassword;
-    setUserPasswordRepeat = hooks.setUserPasswordRepeat;
+    setUserAuthState = jest.fn().mockImplementation((userAuthState) => userAuthState);
+    setUserEmail = jest.fn().mockImplementation((userEmail) => userEmail);
+    setUserPassword = jest.fn().mockImplementation((userEmail) => userEmail);
+    setUserPasswordRepeat = jest.fn().mockImplementation((userEmail) => userEmail);
 }
