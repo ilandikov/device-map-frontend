73627a6ce285650ecb62ce64222fccefd4618178
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-20 14:33:18 +0600

    test: ! set hook mocks values at the declaration

--- src/components/website/login/__test__/LoginModalTestHelpers.ts
+++ src/components/website/login/__test__/LoginModalTestHelpers.ts
@@ -1,10 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-export let setUserAuthState: jest.Mock;
-export let setUserEmail: jest.Mock;
-export let setUserPassword: jest.Mock;
-export let setUserPasswordRepeat: jest.Mock;
+export let setUserAuthState = jest.fn().mockImplementation((userAuthState) => userAuthState);
+export let setUserEmail = jest.fn().mockImplementation((userEmail) => userEmail);
+export let setUserPassword = jest.fn().mockImplementation((userEmail) => userEmail);
+export let setUserPasswordRepeat = jest.fn().mockImplementation((userEmail) => userEmail);
 
 export function resetHookMocks() {
     setUserAuthState = jest.fn().mockImplementation((userAuthState) => userAuthState);
