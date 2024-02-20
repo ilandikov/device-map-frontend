63b85a4b550f6b065379a42422dcee9b38a6eafc
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-20 14:35:12 +0600

    test: ! empty resetHookMocks()

--- src/components/website/login/__test__/LoginModalTestHelpers.ts
+++ src/components/website/login/__test__/LoginModalTestHelpers.ts
@@ -1,17 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-export let setUserAuthState = jest.fn().mockImplementation((userAuthState) => userAuthState);
-export let setUserEmail = jest.fn().mockImplementation((userEmail) => userEmail);
-export let setUserPassword = jest.fn().mockImplementation((userEmail) => userEmail);
-export let setUserPasswordRepeat = jest.fn().mockImplementation((userEmail) => userEmail);
+export const setUserAuthState = jest.fn().mockImplementation((userAuthState) => userAuthState);
+export const setUserEmail = jest.fn().mockImplementation((userEmail) => userEmail);
+export const setUserPassword = jest.fn().mockImplementation((userEmail) => userEmail);
+export const setUserPasswordRepeat = jest.fn().mockImplementation((userEmail) => userEmail);
 
-export function resetHookMocks() {
-    setUserAuthState = jest.fn().mockImplementation((userAuthState) => userAuthState);
-    setUserEmail = jest.fn().mockImplementation((userEmail) => userEmail);
-    setUserPassword = jest.fn().mockImplementation((userEmail) => userEmail);
-    setUserPasswordRepeat = jest.fn().mockImplementation((userEmail) => userEmail);
-}
+export function resetHookMocks() {}
 
 export function renderAsJSON(component: React.ReactElement) {
     return renderer.create(component).toJSON();
