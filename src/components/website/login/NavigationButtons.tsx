d8a71bac8f9618fe1b667d33b05a7bb2d40ab59d
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-21 12:51:17 +0600

    refactor: - rename CSS class to reflect the image properties and not the button

--- src/components/website/login/NavigationButtons.tsx
+++ src/components/website/login/NavigationButtons.tsx
@@ -13,7 +13,11 @@ export function NavigationButtons(props: { setUserAuthState: (string) => void; g
                     props.setUserAuthState(props.goBackState);
                 }}
             >
-                <img className="login-modal-navigation-button" src={GoBack} alt="login-modal-navigation-go-back" />
+                <img
+                    className="login-modal-navigation-button-image"
+                    src={GoBack}
+                    alt="login-modal-navigation-go-back"
+                />
             </button>
             <button
                 data-testid="cancelButton"
@@ -21,7 +25,7 @@ export function NavigationButtons(props: { setUserAuthState: (string) => void; g
                     props.setUserAuthState(UserAuthState.WELCOME);
                 }}
             >
-                <img className="login-modal-navigation-button" src={Cancel} alt="login-modal-navigation-cancel" />
+                <img className="login-modal-navigation-button-image" src={Cancel} alt="login-modal-navigation-cancel" />
             </button>
         </header>
     );
