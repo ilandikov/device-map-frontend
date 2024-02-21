e488950b4a771ac2bc2da8cb62835f3ed1e44369
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-21 12:48:53 +0600

    refactor: - add button tag for navigation buttons

--- src/components/website/login/NavigationButtons.tsx
+++ src/components/website/login/NavigationButtons.tsx
@@ -7,24 +7,28 @@ import { UserAuthState } from './LoginModal';
 export function NavigationButtons(props: { setUserAuthState: (string) => void; goBackState: UserAuthState }) {
     return (
         <header className="login-modal-navigation-container">
-            <img
-                className="login-modal-navigation-button"
-                src={GoBack}
-                alt="login-modal-navigation-go-back"
-                data-testid="goBackButton"
-                onClick={() => {
-                    props.setUserAuthState(props.goBackState);
-                }}
-            />
-            <img
-                className="login-modal-navigation-button"
-                src={Cancel}
-                alt="login-modal-navigation-cancel"
-                data-testid="cancelButton"
-                onClick={() => {
-                    props.setUserAuthState(UserAuthState.WELCOME);
-                }}
-            />
+            <button>
+                <img
+                    className="login-modal-navigation-button"
+                    src={GoBack}
+                    alt="login-modal-navigation-go-back"
+                    data-testid="goBackButton"
+                    onClick={() => {
+                        props.setUserAuthState(props.goBackState);
+                    }}
+                />
+            </button>
+            <button>
+                <img
+                    className="login-modal-navigation-button"
+                    src={Cancel}
+                    alt="login-modal-navigation-cancel"
+                    data-testid="cancelButton"
+                    onClick={() => {
+                        props.setUserAuthState(UserAuthState.WELCOME);
+                    }}
+                />
+            </button>
         </header>
     );
 }
