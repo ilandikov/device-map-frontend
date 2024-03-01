0f8dde4104ef37c7059503c56e7476010c6baa97
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-01 18:17:02 +0600

    feat: - draw product description

--- src/components/website/mapApp/MapApp.tsx
+++ src/components/website/mapApp/MapApp.tsx
@@ -1,16 +1,26 @@
 /* External dependencies */
 import React from 'react';
+import { useI18next } from 'gatsby-plugin-react-i18next';
 
 /* Local dependencies */
 import './MapApp.scss';
 import mapImage from '../../../assets/images/GoogleMaps.png';
+import phoneCut from '../../../assets/images/Phone_Cut.png';
 import { LoginModal } from '../login/LoginModal';
 import { MapAppHeader } from './MapAppHeader';
 
 export default function MapApp() {
+    const { t } = useI18next();
     return (
         <div className="map-app-container">
             <MapAppHeader />
+            <div className="map-app-product-description">
+                <div className="map-app-product-description-text">
+                    <h1>{`${t('brand')} ${t('map')}`}</h1>
+                    <p>{t('loginCallToAction')}</p>
+                </div>
+                <img src={phoneCut} alt="map-app-description-image" />
+            </div>
             <LoginModal />
             <img className="map-app-map-image" src={mapImage} alt="map" />
         </div>
