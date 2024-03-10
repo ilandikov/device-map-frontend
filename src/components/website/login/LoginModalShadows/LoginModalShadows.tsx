db9c0f87a95a429226049617d136de462d35fab3
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-10 13:56:11 +0600

    refactor: ! remove excessive containers in LoginModalShadows()

--- src/components/website/login/LoginModalShadows/LoginModalShadows.tsx
+++ src/components/website/login/LoginModalShadows/LoginModalShadows.tsx
@@ -3,13 +3,9 @@ import './LoginModalShadows.scss';
 
 export function LoginModalShadows() {
     return (
-        <div className="login-modal-shadows-container">
-            <div className="login-modal-shadows-left-container">
-                <div className="login-modal-shadow login-modal-shadow-green"></div>
-            </div>
-            <div className="login-modal-shadows-right-container">
-                <div className="login-modal-shadow login-modal-shadow-blue"></div>
-            </div>
-        </div>
+        <>
+            <div className="login-modal-shadow login-modal-shadow-green"></div>
+            <div className="login-modal-shadow login-modal-shadow-blue"></div>
+        </>
     );
 }
