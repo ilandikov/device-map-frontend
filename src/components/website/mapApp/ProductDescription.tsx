a2029c048d558800906c159b9351d55b50075ad8
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-11 18:13:47 +0600

    refactor: . extract ProductDescription() component

---
+++ src/components/website/mapApp/ProductDescription.tsx
@@ -0,0 +1,19 @@
+import { useI18next } from 'gatsby-plugin-react-i18next';
+import React from 'react';
+import phoneCut from '../../../assets/images/Phone_Cut.png';
+
+export function ProductDescription() {
+    const { t } = useI18next();
+    return (
+        <div className="map-app-product-description">
+            <div className="map-app-product-description-shadow" />
+            <div className="map-app-product-description-container">
+                <div className="map-app-product-description-text">
+                    <h1>{`${t('brand')} ${t('map')}`}</h1>
+                    <p>{t('loginCallToAction')}</p>
+                </div>
+                <img src={phoneCut} alt="map-app-description-image" />
+            </div>
+        </div>
+    );
+}
