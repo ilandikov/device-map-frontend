308a67e25b317da5a065a7f2dad0370487f371c2
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-12 13:56:20 +0600

    refactor: - take state into account to draw ProductDescription()

--- src/components/website/mapApp/MapApp.tsx
+++ src/components/website/mapApp/MapApp.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 
 /* Local dependencies */
 import './MapApp.scss';
+import { useSelector } from 'react-redux';
 import mapImage from '../../../assets/images/GoogleMaps.png';
+import { RootState } from '../../../redux/store';
 import { MapAppHeader } from './MapAppHeader';
 import { ProductDescription } from './ProductDescription';
+import { MapAppState } from './MapAppReducer';
 
 export default function MapApp() {
+    const mapAppState: MapAppState = useSelector((state: RootState) => state.mapAppState);
     return (
         <div className="map-app-container">
             <MapAppHeader />
-            <ProductDescription />
+            {mapAppState.showProductDescription && <ProductDescription />}
             <img className="map-app-map-image" src={mapImage} alt="map" />
         </div>
     );
