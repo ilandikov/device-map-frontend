588c24462d803bc2746f7f19f46c56d83f60b9e3
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-13 13:48:17 +0600

    test: - test with toHaveBeenNthCalledWith()

--- src/components/website/login/__test__/NavigationButtons.test.tsx
+++ src/components/website/login/__test__/NavigationButtons.test.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { configureTestStore } from '../../../../../tests/utils';
 import { NavigationButtons } from '../NavigationButtons';
 import { UserAuthState } from '../LoginModal';
-import { setUserAuthState } from './LoginModalTestHelpers';
+import { resetLoginModalMocks, setUserAuthState } from './LoginModalTestHelpers';
 
 const store = configureTestStore();
 
@@ -17,13 +17,17 @@ function componentWithStoreProvider(goBackState: UserAuthState) {
 }
 
 describe('Navigation buttons tests', () => {
+    beforeEach(() => {
+        resetLoginModalMocks();
+    });
+
     it('should go back to welcome stage on cancel button click', () => {
         const { container } = componentWithStoreProvider(UserAuthState.WELCOME);
 
         const cancelButton = getByTestId(container, 'cancelButton');
         fireEvent.click(cancelButton);
 
-        expect(setUserAuthState).toHaveBeenCalledWith(UserAuthState.WELCOME);
+        expect(setUserAuthState).toHaveBeenNthCalledWith(1, UserAuthState.WELCOME);
     });
 
     it('should go back to a desired go back state on go back button click', () => {
@@ -32,6 +36,6 @@ describe('Navigation buttons tests', () => {
         const goBackButton = getByTestId(container, 'goBackButton');
         fireEvent.click(goBackButton);
 
-        expect(setUserAuthState).toHaveBeenCalledWith(UserAuthState.MAIL_INPUT);
+        expect(setUserAuthState).toHaveBeenNthCalledWith(1, UserAuthState.MAIL_INPUT);
     });
 });
