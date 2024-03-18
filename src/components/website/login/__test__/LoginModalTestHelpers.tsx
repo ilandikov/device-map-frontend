6010ae6b539bfd536a06c054ed9fe5edbc54e40d
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-18 18:15:55 +0600

    test: . inline renderAsJSON()

--- src/components/website/login/__test__/LoginModalTestHelpers.tsx
+++ src/components/website/login/__test__/LoginModalTestHelpers.tsx
@@ -4,16 +4,12 @@ import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import { configureTestStore } from '../../../../../tests/utils';
 
-export function renderAsJSON(component: React.ReactElement) {
-    return renderer.create(component).toJSON();
-}
-
 export function componentWithStoreProvider(component: React.JSX.Element) {
     return <Provider store={configureTestStore()}>{component}</Provider>;
 }
 
 export function renderForSnapshotTest(component: React.JSX.Element) {
-    return renderAsJSON(componentWithStoreProvider(component));
+    return renderer.create(componentWithStoreProvider(component)).toJSON();
 }
 
 export function renderForActionDispatchTest(component: React.JSX.Element) {
