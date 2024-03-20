8aa696dd75bcd362d109c6d5a528a811237bbf9a
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-20 16:52:19 +0600

    test: - mock MapAppState and use the mock

--- src/components/website/mapApp/__test__/MapApp.test.tsx
+++ src/components/website/mapApp/__test__/MapApp.test.tsx
@@ -3,9 +3,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import MapApp from '../MapApp';
 import { configureTestStore } from '../../../../../tests/utils';
+import { mockDispatch, mockMapAppState, mockPrepareSelector } from '../redux/__mocks__/MapAppState';
+import { MapAppUsageStep } from '../MapAppReducer';
+
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useDispatch: () => mockDispatch,
+    useSelector: () => mockPrepareSelector(),
+}));
 
 describe('MapApp snapshot tests', () => {
     it('should match the snapshot', () => {
+        mockMapAppState({ usageStep: MapAppUsageStep.HOMESCREEN, showProductDescription: true, showLoginModal: false });
         const mapAppWithTestStore = (
             <Provider store={configureTestStore()}>
                 <MapApp />
