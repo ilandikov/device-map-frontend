496126b90ae82428f2943d1772c2dd5e32d11926
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-30 00:34:53 +0600

    dispatch 2 actions

--- src/components/website/login/redux/epic.ts
+++ src/components/website/login/redux/epic.ts
@@ -1,63 +1,12 @@
-import { Observable, switchMap } from 'rxjs';
-import CognitoClient from '@mancho.devs/cognito';
+import { Observable, of, switchMap } from 'rxjs';
 import { ofType } from 'redux-observable';
-import {
-    LoginModalAction,
-    LoginModalActionTypes,
-    LoginModalNotificationTypes,
-    LoginModalVerifyRequest,
-    LoginModalVerifyTypes,
-    loginModalFailureNotification,
-    loginModalNoAction,
-    loginModalSuccessNotification,
-} from './actions';
-import { AuthenticationState } from './state';
-import { buildMessageFromCognitoException } from './epicHelpers';
+import { LoginModalAction, LoginModalActionTypes, loginModalNoAction } from './actions';
 
-const cognitoClient = new CognitoClient({
-    UserPoolId: process.env.GATSBY_COGNITO_USER_POOL_ID,
-    ClientId: process.env.GATSBY_COGNITO_CLIENT_ID,
-});
-
-export function cognito(action$, state$): Observable<LoginModalAction> {
+export function cognito(action$): Observable<LoginModalAction> {
     return action$.pipe(
         ofType(LoginModalActionTypes.VERIFY_REQUEST),
-        switchMap(async (action: LoginModalVerifyRequest) => {
-            const authenticationState: AuthenticationState = state$.value.authentication;
-            if (authenticationState.error !== null) {
-                return loginModalNoAction();
-            }
-
-            switch (action.verify) {
-                case LoginModalVerifyTypes.USER_PASSWORD: {
-                    return cognitoClient
-                        .signUp(authenticationState.email, authenticationState.password)
-                        .then(() => {
-                            return loginModalSuccessNotification(LoginModalNotificationTypes.SIGNUP);
-                        })
-                        .catch((reason) => {
-                            return loginModalFailureNotification(
-                                LoginModalNotificationTypes.SIGNUP,
-                                buildMessageFromCognitoException(reason),
-                            );
-                        });
-                }
-                case LoginModalVerifyTypes.OTP: {
-                    return cognitoClient
-                        .signUpConfirmCode(authenticationState.email, authenticationState.OTP)
-                        .then(() => {
-                            return loginModalSuccessNotification(LoginModalNotificationTypes.OTP);
-                        })
-                        .catch((reason) => {
-                            return loginModalFailureNotification(
-                                LoginModalNotificationTypes.OTP,
-                                buildMessageFromCognitoException(reason),
-                            );
-                        });
-                }
-            }
-
-            return loginModalNoAction();
+        switchMap(() => {
+            return of(loginModalNoAction(), loginModalNoAction());
         }),
     );
 }
