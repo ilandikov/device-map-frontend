715f7fc2125a7dbbb9e433cfab3e5afa87bef043
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-04-03 15:49:01 +0600

    test: - reuse renderForSnapshotTest() for MainPage() snapshot test

--- src/components/website/home/__test__/MainPage.test.tsx
+++ src/components/website/home/__test__/MainPage.test.tsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import { I18nextProvider } from 'gatsby-plugin-react-i18next';
-import { Provider } from 'react-redux';
-import renderer, { act } from 'react-test-renderer';
-import i18n from '../../../../../tests/utils/i18nForTest';
-import { configureTestStore } from '../../../../../tests/utils';
+import { renderForSnapshotTest } from '../../../../../tests/utils/RenderingHelpers';
 import MainPage from '../MainPage';
 
 describe('MainPage component', () => {
-    function getRenderComponent() {
-        return (
-            <Provider store={configureTestStore()}>
-                <I18nextProvider i18n={i18n}>
-                    <MainPage />
-                </I18nextProvider>
-            </Provider>
-        );
-    }
+    it('should match the snapshot', () => {
+        const component = renderForSnapshotTest(<MainPage />);
 
-    it('should match the snapshot', async () => {
-        let component;
-
-        await act(async () => {
-            component = renderer.create(getRenderComponent());
-        });
-
-        await new Promise((resolve) => setTimeout(resolve, 0));
-
-        expect(component.toJSON()).toMatchSnapshot();
+        expect(component).toMatchSnapshot();
     });
 });
