794e24e4ea939065dccd7ae9ec36310535f20e6c
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-04-11 17:26:06 +0600

    refactor: - reuse mapAppLoginModalOpen()

--- src/components/website/mapApp/__test__/MapAppHeader.test.tsx
+++ src/components/website/mapApp/__test__/MapAppHeader.test.tsx
@@ -3,7 +3,7 @@ import { fireEvent, getByTestId } from '@testing-library/react';
 import { MapAppHeader } from '../MapAppHeader';
 import { mockDispatch } from '../redux/__mocks__/MapAppState';
 import { renderForActionDispatchTest } from '../../../../../tests/utils/RenderingHelpers';
-import { MapAppActionTypes } from '../redux/actions';
+import { mapAppLoginModalOpen } from '../redux/actions';
 
 jest.mock('react-redux', () => ({
     ...jest.requireActual('react-redux'),
@@ -21,6 +21,6 @@ describe('MapAppHeader action tests', () => {
         const loginButton = getByTestId(container, 'loginButton');
         fireEvent.click(loginButton);
 
-        expect(mockDispatch).toHaveBeenNthCalledWith(1, { type: MapAppActionTypes.LOGIN_MODAL_OPEN });
+        expect(mockDispatch).toHaveBeenNthCalledWith(1, mapAppLoginModalOpen());
     });
 });
