79eaaa9fc1da338c68c1ad6b52d984270a5541de
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-04-11 15:28:54 +0600

    test: - port other tests on the test double

--- src/components/website/login/redux/__test__/epic.test.ts
+++ src/components/website/login/redux/__test__/epic.test.ts
@@ -35,6 +35,18 @@ class cognitoTestClient {
     signUp() {
         return this.mockedPromise();
     }
+    signUpConfirmCode() {
+        return this.mockedPromise();
+    }
+    confirmPassword() {
+        return this.mockedPromise();
+    }
+    signIn() {
+        return this.mockedPromise();
+    }
+    forgotPassword() {
+        return this.mockedPromise();
+    }
 
     private mockedPromise() {
         if (this._mockedResult === PromiseResult.REJECT) {
@@ -85,43 +97,38 @@ describe('user sign up tests', () => {
     );
 
     it.each([
-        [Promise.resolve(), loginModalSuccessNotification(LoginModalNotificationTypes.PASSWORD_RESET)],
+        [PromiseResult.RESOLVE, loginModalSuccessNotification(LoginModalNotificationTypes.PASSWORD_RESET)],
         [
-            Promise.reject(),
+            PromiseResult.REJECT,
             loginModalFailureNotification(LoginModalNotificationTypes.PASSWORD_RESET, 'cognitoUnknownException'),
         ],
     ])(
         'should dispatch password has been reset notification when remote answer is: %s',
         async (remoteServiceAnswer, expectedAction) => {
-            jest.spyOn(CognitoClient.prototype, 'confirmPassword').mockImplementation(async (): Promise<any> => {
-                return remoteServiceAnswer;
-            });
-
-            const initialState = buildAuthenticationStateForEpic({
+            const initialState = buildAuthenticationState({
                 step: AuthenticationStep.PASSWORD_RESET_LOADING,
             });
             const sentAction = loginModalVerifyRequest(LoginModalVerifyTypes.PASSWORD);
 
-            await verifyCognitoEpic(sentAction, initialState, expectedAction, { cognitoClient });
+            await verifyCognitoEpic2(sentAction, initialState, remoteServiceAnswer, expectedAction);
         },
     );
 });
 
 describe('user sign up OTP code confirmation tests (from password creation loading step)', () => {
     it.each([
-        [Promise.resolve(), loginModalSuccessNotification(LoginModalNotificationTypes.OTP)],
-        [Promise.reject(), loginModalFailureNotification(LoginModalNotificationTypes.OTP, 'cognitoUnknownException')],
+        [PromiseResult.RESOLVE, loginModalSuccessNotification(LoginModalNotificationTypes.OTP)],
+        [
+            PromiseResult.REJECT,
+            loginModalFailureNotification(LoginModalNotificationTypes.OTP, 'cognitoUnknownException'),
+        ],
     ])('should dispatch OTP notification when remote answer is: %s', async (remoteServiceAnswer, expectedAction) => {
-        jest.spyOn(CognitoClient.prototype, 'signUpConfirmCode').mockImplementation(async (): Promise<any> => {
-            return remoteServiceAnswer;
-        });
-
-        const initialState = buildAuthenticationStateForEpic({
+        const initialState = buildAuthenticationState({
             step: AuthenticationStep.PASSWORD_CREATION_OTP_LOADING,
         });
         const sentAction = loginModalVerifyRequest(LoginModalVerifyTypes.OTP);
 
-        await verifyCognitoEpic(sentAction, initialState, expectedAction, { cognitoClient });
+        await verifyCognitoEpic2(sentAction, initialState, remoteServiceAnswer, expectedAction);
     });
 
     it('should dispatch no action and not call signUp() from password reset loading step', async () => {
@@ -136,26 +143,21 @@ describe('user sign up OTP code confirmation tests (from password creation loadi
 
 describe('user sign in tests', () => {
     it.each([
-        [Promise.resolve(), loginModalSuccessNotification(LoginModalNotificationTypes.SIGN_IN)],
+        [PromiseResult.RESOLVE, loginModalSuccessNotification(LoginModalNotificationTypes.SIGN_IN)],
         [
-            Promise.reject(),
+            PromiseResult.REJECT,
             loginModalFailureNotification(LoginModalNotificationTypes.SIGN_IN, 'cognitoUnknownException'),
         ],
     ])('should dispatch login notification when remote answer is: %s', async (remoteServiceAnswer, expectedAction) => {
-        jest.spyOn(CognitoClient.prototype, 'signIn').mockImplementation(async (): Promise<any> => {
-            return remoteServiceAnswer;
-        });
-
-        const initialState = buildAuthenticationStateForEpic({ step: AuthenticationStep.LOGIN_LOADING });
+        const initialState = buildAuthenticationState({ step: AuthenticationStep.LOGIN_LOADING });
         const sentAction = loginModalVerifyRequest(LoginModalVerifyTypes.EMAIL_AND_PASSWORD);
 
-        await verifyCognitoEpic(sentAction, initialState, expectedAction, { cognitoClient });
+        await verifyCognitoEpic2(sentAction, initialState, remoteServiceAnswer, expectedAction);
     });
 });
 
 describe('password reset tests', () => {
     it('should not call cognito service on email verification during mail input step', async () => {
-        jest.spyOn(CognitoClient.prototype, 'forgotPassword').mockImplementation(() => Promise.resolve());
         const initialState = buildAuthenticationStateForEpic({ step: AuthenticationStep.MAIL_INPUT });
         const sentAction = loginModalVerifyRequest(LoginModalVerifyTypes.EMAIL);
 
@@ -163,22 +165,18 @@ describe('password reset tests', () => {
     });
 
     it.each([
-        [Promise.resolve(), loginModalSuccessNotification(LoginModalNotificationTypes.FORGOT_PASSWORD)],
+        [PromiseResult.RESOLVE, loginModalSuccessNotification(LoginModalNotificationTypes.FORGOT_PASSWORD)],
         [
-            Promise.reject(),
+            PromiseResult.REJECT,
             loginModalFailureNotification(LoginModalNotificationTypes.FORGOT_PASSWORD, 'cognitoUnknownException'),
         ],
     ])(
         'should dispatch forgot password notification when remote answer is: %s',
         async (remoteServiceAnswer, expectedAction) => {
-            jest.spyOn(CognitoClient.prototype, 'forgotPassword').mockImplementation(async (): Promise<any> => {
-                return remoteServiceAnswer;
-            });
-
-            const initialState = buildAuthenticationStateForEpic({ step: AuthenticationStep.PASSWORD_RESET_LOADING });
+            const initialState = buildAuthenticationState({ step: AuthenticationStep.PASSWORD_RESET_LOADING });
             const sentAction = loginModalVerifyRequest(LoginModalVerifyTypes.EMAIL);
 
-            await verifyCognitoEpic(sentAction, initialState, expectedAction, { cognitoClient });
+            await verifyCognitoEpic2(sentAction, initialState, remoteServiceAnswer, expectedAction);
         },
     );
 });
