5ababe35be6ccfa11d6b28b9c016ebf27ac852d9
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-04-11 17:11:34 +0600

    refactor: . extract MapAppActionInterface

--- src/components/website/mapApp/redux/actions.ts
+++ src/components/website/mapApp/redux/actions.ts
@@ -1,5 +1,9 @@
 import { MapAppActionTypes } from '../MapAppReducer';
 
-export function mapAppLoginModalClose() {
+interface MapAppAction {
+    type: MapAppActionTypes;
+}
+
+export function mapAppLoginModalClose(): MapAppAction {
     return { type: MapAppActionTypes.LOGIN_MODAL_OPEN };
 }
