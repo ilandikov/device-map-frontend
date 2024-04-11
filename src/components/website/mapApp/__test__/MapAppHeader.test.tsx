afbff3ef6838c7ed33021ce37c335ccf15980b08
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-04-11 22:32:51 +0600

    test: - test user authenticated snapshots in MapAppHeader() tests

--- src/components/website/mapApp/__test__/MapAppHeader.test.tsx
+++ src/components/website/mapApp/__test__/MapAppHeader.test.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { fireEvent, getByTestId } from '@testing-library/react';
 import { MapAppHeader } from '../MapAppHeader';
-import { mockDispatch } from '../redux/__mocks__/MapAppState';
-import { renderForActionDispatchTest } from '../../../../../tests/utils/RenderingHelpers';
+import { mockDispatch, mockMapAppState, mockPrepareSelector } from '../redux/__mocks__/MapAppState';
+import { renderForActionDispatchTest, renderForSnapshotTest } from '../../../../../tests/utils/RenderingHelpers';
 import { mapAppLoginModalOpen } from '../redux/actions';
+import { MapAppUsageStep } from '../redux/MapAppReducer';
 
 jest.mock('react-redux', () => ({
     ...jest.requireActual('react-redux'),
     useDispatch: () => mockDispatch,
+    useSelector: () => mockPrepareSelector(),
 }));
+
+describe('MapAppHeader snapshot tests', () => {
+    it('should match the snapshot at the initial state', () => {
+        mockMapAppState({});
+        const component = renderForSnapshotTest(<MapAppHeader />);
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should match the snapshot at user authenticated state', () => {
+        mockMapAppState({
+            usageStep: MapAppUsageStep.AUTHENTICATED_USER,
+        });
+        const component = renderForSnapshotTest(<MapAppHeader />);
+
+        expect(component).toMatchSnapshot();
+    });
+});
 
 describe('MapAppHeader action tests', () => {
     beforeEach(() => {
