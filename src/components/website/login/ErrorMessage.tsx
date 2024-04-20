0b026b159754192faef5ce29c2bd1bfbd96d5086
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-04-20 19:42:00 +0600

    fix: ! translate error messages

--- src/components/website/login/ErrorMessage.tsx
+++ src/components/website/login/ErrorMessage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useI18next } from 'gatsby-plugin-react-i18next';
 
 export function ErrorMessage(props: { error: Error }) {
+    const { t } = useI18next();
     return (
-        <>{props.error && <p className="login-modal-input-help login-modal-wrong-input">{props.error.message}</p>}</>
+        <>{props.error && <p className="login-modal-input-help login-modal-wrong-input">{t(props.error.message)}</p>}</>
     );
 }
