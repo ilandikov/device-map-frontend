c13d0195b3f0d36fbe03f9acb13feddecdbd5633
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-04-22 18:36:01 +0600

    feat: - add background video

--- src/components/website/mainPage/Map.tsx
+++ src/components/website/mainPage/Map.tsx
@@ -8,10 +8,16 @@ import Logo from '/src/assets/images/Logo.svg';
 import WebApp from '/src/assets/images/WebApp.svg';
 import GooglePlay from '/src/assets/images/GooglePlayOpaque.svg';
 import AppStore from '/src/assets/images/AppStoreOpaque.svg';
+import Kyrgyzstan from '/src/assets/video/Kyrgyzstan.mp4';
 
 export default function Map() {
     return (
         <section className="map-background">
+            <div className="map-background-video">
+                <video autoPlay loop muted>
+                    <source src={Kyrgyzstan} type="video/mp4" className="map-background-video-source" />
+                </video>
+            </div>
             <div className="map-content-container">
                 <div className="map-brand">
                     <img src={Logo} alt="logo-with-brand" />
