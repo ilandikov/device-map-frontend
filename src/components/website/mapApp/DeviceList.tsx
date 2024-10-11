a7c3d31dbf0e8030c7a880a6a6ba80c78008f074
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-12 01:24:43 +0600

    refactor: . move device list item to a separate file

--- src/components/website/mapApp/DeviceList.tsx
+++ src/components/website/mapApp/DeviceList.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import { useI18next } from 'gatsby-plugin-react-i18next';
 import home from '../../../assets/images/Home.png';
-import terminals from '../../../assets/images/Terminals.png';
-import { Device, useMapAppState } from './redux/MapAppState';
+import { useMapAppState } from './redux/MapAppState';
 import './DeviceList.scss';
-
-function DeviceListItem(device: Device) {
-    const { t } = useI18next();
-
-    return (
-        <div className="devices-list-item-container">
-            <div className="devices-list-item-shadow devices-list-item-shadow-left"></div>
-            <div className="devices-list-item-shadow devices-list-item-shadow-right"></div>
-            <img src={terminals} className="devices-list-item-image" alt="devices-list-item-image" />
-            <div className="devices-list-item">
-                <p>{device.name}</p>
-                <button>{t('deviceReportBroken')}</button>
-            </div>
-        </div>
-    );
-}
+import { DeviceListItem } from './DeviceListItem';
 
 export function DeviceList() {
     const mapAppState = useMapAppState();
