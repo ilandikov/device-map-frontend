2b73efb314297ae64696fd65c55e5a82d3afa365
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-12 01:23:23 +0600

    feat: - draw device item according to design

--- src/components/website/mapApp/DeviceList.tsx
+++ src/components/website/mapApp/DeviceList.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import home from '../../../assets/images/Home.png';
+import terminals from '../../../assets/images/Terminals.png';
 import { Device, useMapAppState } from './redux/MapAppState';
+import './DeviceList.scss';
 
 function DeviceListItem(device: Device) {
     const { t } = useI18next();
 
     return (
-        <div className="devices-list-item">
-            <p>{device.name}</p>
-            <button>{t('deviceReportBroken')}</button>
+        <div className="devices-list-item-container">
+            <div className="devices-list-item-shadow devices-list-item-shadow-left"></div>
+            <div className="devices-list-item-shadow devices-list-item-shadow-right"></div>
+            <img src={terminals} className="devices-list-item-image" alt="devices-list-item-image" />
+            <div className="devices-list-item">
+                <p>{device.name}</p>
+                <button>{t('deviceReportBroken')}</button>
+            </div>
         </div>
     );
 }
@@ -31,9 +38,9 @@ export function DeviceList() {
     return (
         <div className="devices-list-window">
             <div className="devices-list-address-container">
-                <img src={home} alt="devices-list-address-image" />
+                <img src={home} className="devices-list-address-image" alt="devices-list-address-image" />
                 <div className="devices-address">
-                    <h2>Address1</h2>
+                    <p>Address1</p>
                     <span>Address2</span>
                 </div>
             </div>
