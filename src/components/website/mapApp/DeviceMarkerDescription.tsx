05ee70f941a4723218e334430cdf8c1e045db505
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-12 21:20:58 +0600

    refactor: - rearrange MarkerAddress component

--- src/components/website/mapApp/DeviceMarkerDescription.tsx
+++ src/components/website/mapApp/DeviceMarkerDescription.tsx
@@ -1,29 +1,12 @@
-import React, { useEffect } from 'react';
-import home from '../../../assets/images/Home.png';
-import { useAppDispatch } from '../../../redux/store';
-import { useMapAppState } from './redux/MapAppState';
+import React from 'react';
 import './DeviceMarkerDescription.scss';
-import { mapAppGetLocationAddress } from './redux/MapAppAction';
-import { MarkerAddressLoader } from './MarkerAddressLoader';
-import { MarkerAddress } from './MarkerAddress';
 import { DeviceList } from './DeviceList';
+import { MarkerAddress } from './MarkerAddress';
 
 export function DeviceMarkerDescription() {
-    const dispatch = useAppDispatch();
-
-    const selectedMarker = useMapAppState().selectedMarker;
-    const waitingForAddress = selectedMarker.address === null;
-
-    useEffect(() => {
-        dispatch(mapAppGetLocationAddress(selectedMarker.location));
-    }, [selectedMarker.location]);
-
     return (
         <div className="device-marker-description-window">
-            <div className="marker-address-container">
-                <img src={home} className="marker-address-image" alt="marker-address-image" />
-                {waitingForAddress ? <MarkerAddressLoader /> : <MarkerAddress address={selectedMarker.address} />}
-            </div>
+            <MarkerAddress />
             <DeviceList />
         </div>
     );
--- src/components/website/mapApp/MarkerAddress.tsx
+++ src/components/website/mapApp/MarkerAddress.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { MapAppAddress } from './redux/MapAppState';
+import React, { useEffect } from 'react';
+import { useAppDispatch } from '../../../redux/store';
+import home from '../../../assets/images/Home.png';
+import { useMapAppState } from './redux/MapAppState';
+import { mapAppGetLocationAddress } from './redux/MapAppAction';
+import { MarkerAddressLoader } from './MarkerAddressLoader';
 import './MarkerAddress.scss';
 
-export function MarkerAddress(props: { address: MapAppAddress }) {
+export function MarkerAddress() {
+    const selectedMarker = useMapAppState().selectedMarker;
+    const waitingForAddress = selectedMarker.address === null;
+
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(mapAppGetLocationAddress(selectedMarker.location));
+    }, [selectedMarker.location]);
+
     return (
-        <div className="marker-address">
-            <p>{props.address.addressLine1}</p>
-            <span>{props.address.addressLine2}</span>
+        <div className="marker-address-container">
+            <img src={home} className="marker-address-image" alt="marker-address-image" />
+            {waitingForAddress ? (
+                <MarkerAddressLoader />
+            ) : (
+                <div className="marker-address">
+                    <p>{selectedMarker.address.addressLine1}</p>
+                    <span>{selectedMarker.address.addressLine2}</span>
+                </div>
+            )}
         </div>
     );
 }
