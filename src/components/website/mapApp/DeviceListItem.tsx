976ee09d276fe37eca2dc557a6d19e29fd08cc3f
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-13 01:12:04 +0600

    refactor: - extract shadow colors to a separate class

--- src/components/website/mapApp/DeviceListItem.tsx
+++ src/components/website/mapApp/DeviceListItem.tsx
@@ -8,8 +8,8 @@ export function DeviceListItem(props: { device: Device }) {
 
     return (
         <div className="device-list-item-container">
-            <div className="device-list-item-shadow device-list-item-shadow-left"></div>
-            <div className="device-list-item-shadow device-list-item-shadow-right"></div>
+            <div className="device-list-item-shadow device-list-item-shadow-left device-list-item-shadow-left-first"></div>
+            <div className="device-list-item-shadow device-list-item-shadow-right device-list-item-shadow-right-first"></div>
             <img src={terminals} className="device-list-item-image" alt="device-list-item-image" />
             <div className="device-list-item">
                 <p>{props.device.name}</p>
