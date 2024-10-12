2fdbc350b9ac37790cdf0913f2939b06989b2ffd
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-13 01:18:18 +0600

    refactor: ! add additional devices to show different colors in future

--- src/components/website/mapApp/redux/MapAppState.ts
+++ src/components/website/mapApp/redux/MapAppState.ts
@@ -41,6 +41,8 @@ export const mapAppInitialState: MapAppState = {
         { name: 'dev1', location: { lat: 42.85862508449081, lng: 74.6085298061371 } },
         { name: 'dev2a', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
         { name: 'dev2b', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+        { name: 'dev2c', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+        { name: 'dev2d', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
         { name: 'dev3', location: { lat: 42.85610049481582, lng: 74.60671663284303 } },
     ],
     selectedMarker: {
