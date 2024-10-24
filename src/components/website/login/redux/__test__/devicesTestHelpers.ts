d08b78867f829e40e56496baa6280bd40c55aa23
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-24 22:16:42 +0600

    test: . extract buildInitialTestState()

--- src/components/website/login/redux/__test__/devicesTestHelpers.ts
+++ src/components/website/login/redux/__test__/devicesTestHelpers.ts
@@ -5,17 +5,19 @@ import { devices } from '../devices';
 import { initialGetDevicesState } from '../../../../devices/getDevices/redux/reducer';
 import { mapAppInitialState } from '../../../mapApp/redux/MapAppState';
 import { authenticationInitialState } from '../LoginModalAuthenticationState';
+import { RootState } from '../../../../../redux/store';
+
+export function buildInitialTestState(): RootState {
+    return {
+        loginModalAuthentication: authenticationInitialState,
+        getDevices: initialGetDevicesState,
+        mapAppState: mapAppInitialState,
+    };
+}
 
 export async function testDevicesEpic(sentAction: MapAppAction, expectedActions: MapAppAction[]) {
-    const output$ = devices(
-        of(sentAction),
-        new StateObservable(EMPTY, {
-            loginModalAuthentication: authenticationInitialState,
-            getDevices: initialGetDevicesState,
-            mapAppState: mapAppInitialState,
-        }),
-        { cognitoClient: {} },
-    );
+    const initialRootState = buildInitialTestState();
+    const output$ = devices(of(sentAction), new StateObservable(EMPTY, initialRootState), { cognitoClient: {} });
     const receivedAction = await lastValueFrom(output$.pipe(toArray()));
     expect(receivedAction).toEqual(expectedActions);
 }
