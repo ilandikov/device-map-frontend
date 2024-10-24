cfbfdc5b258b1ea34abe7b0659cc5a43a86df187
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-24 16:50:04 +0600

    test: - inline parameter and remove type

--- src/components/website/login/redux/__test__/devices.test.ts
+++ src/components/website/login/redux/__test__/devices.test.ts
@@ -1,9 +1,4 @@
-import {
-    MapAppActionType,
-    MapAppRemoteAnswer,
-    MapAppRemoteRequest,
-    mapAppRemoteAnswer,
-} from '../../../mapApp/redux/MapAppAction';
+import { MapAppActionType, MapAppRemoteRequest, mapAppRemoteAnswer } from '../../../mapApp/redux/MapAppAction';
 import { testDevicesEpic } from './devicesTestHelpers';
 
 describe('devices epic test', () => {
@@ -17,15 +12,16 @@ describe('devices epic test', () => {
 
     it('should get a list of devices', async () => {
         const sentAction: MapAppRemoteRequest = { type: MapAppActionType.REMOTE_REQUEST };
-        const expectedDevices = [
-            { id: 'dev1', location: { lat: 42.85862508449081, lng: 74.6085298061371 } },
-            { id: 'dev2a', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-            { id: 'dev2b', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-            { id: 'dev2c', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-            { id: 'dev2d', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-            { id: 'dev3', location: { lat: 42.85610049481582, lng: 74.60671663284303 } },
+        const expectedActions = [
+            mapAppRemoteAnswer([
+                { id: 'dev1', location: { lat: 42.85862508449081, lng: 74.6085298061371 } },
+                { id: 'dev2a', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                { id: 'dev2b', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                { id: 'dev2c', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                { id: 'dev2d', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                { id: 'dev3', location: { lat: 42.85610049481582, lng: 74.60671663284303 } },
+            ]),
         ];
-        const expectedActions: MapAppRemoteAnswer[] = [mapAppRemoteAnswer(expectedDevices)];
 
         await testDevicesEpic(sentAction, expectedActions);
     });
