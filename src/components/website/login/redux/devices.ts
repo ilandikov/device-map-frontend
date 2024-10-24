ee906b40ca8a25e25d2e5ca7f1a429ba5f6d7268
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-24 16:45:21 +0600

    refactor: - remove unused parameter

--- src/components/website/login/redux/devices.ts
+++ src/components/website/login/redux/devices.ts
@@ -1,11 +1,11 @@
 import { Observable, of, switchMap } from 'rxjs';
 import { ofType } from 'redux-observable';
-import { MapAppAction, MapAppActionType, MapAppRemoteRequest } from '../../mapApp/redux/MapAppAction';
+import { MapAppAction, MapAppActionType } from '../../mapApp/redux/MapAppAction';
 
 export function devices(action$, state$, { _cognitoClient }): Observable<MapAppAction> {
     return action$.pipe(
         ofType(MapAppActionType.REMOTE_REQUEST),
-        switchMap((action: MapAppRemoteRequest) => {
+        switchMap(() => {
             return of({ type: MapAppActionType.REMOTE_ANSWER, devices: [] });
         }),
     );
