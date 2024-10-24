4e09b302627e4d7d3f8d5a788edff664c6dbfd56
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-24 16:46:30 +0600

    refactor: - teach devices to return a devices array

--- src/components/website/login/redux/__test__/devices.test.ts
+++ src/components/website/login/redux/__test__/devices.test.ts
@@ -12,7 +12,19 @@ describe('devices epic test', () => {
 
     it('should get a list of devices', async () => {
         const sentAction: MapAppRemoteRequest = { type: MapAppActionType.REMOTE_REQUEST };
-        const expectedActions: MapAppRemoteAnswer[] = [{ type: MapAppActionType.REMOTE_ANSWER, devices: [] }];
+        const expectedActions: MapAppRemoteAnswer[] = [
+            {
+                type: MapAppActionType.REMOTE_ANSWER,
+                devices: [
+                    { id: 'dev1', location: { lat: 42.85862508449081, lng: 74.6085298061371 } },
+                    { id: 'dev2a', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                    { id: 'dev2b', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                    { id: 'dev2c', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                    { id: 'dev2d', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                    { id: 'dev3', location: { lat: 42.85610049481582, lng: 74.60671663284303 } },
+                ],
+            },
+        ];
 
         await testDevicesEpic(sentAction, expectedActions);
     });
--- src/components/website/login/redux/devices.ts
+++ src/components/website/login/redux/devices.ts
@@ -6,7 +6,17 @@ export function devices(action$, state$, { _cognitoClient }): Observable<MapAppA
     return action$.pipe(
         ofType(MapAppActionType.REMOTE_REQUEST),
         switchMap(() => {
-            return of({ type: MapAppActionType.REMOTE_ANSWER, devices: [] });
+            return of({
+                type: MapAppActionType.REMOTE_ANSWER,
+                devices: [
+                    { id: 'dev1', location: { lat: 42.85862508449081, lng: 74.6085298061371 } },
+                    { id: 'dev2a', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                    { id: 'dev2b', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                    { id: 'dev2c', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                    { id: 'dev2d', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+                    { id: 'dev3', location: { lat: 42.85610049481582, lng: 74.60671663284303 } },
+                ],
+            });
         }),
     );
 }
