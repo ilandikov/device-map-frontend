2ffcc22abab09dbd461889d1f7c798e7ef141a27
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-24 17:01:40 +0600

    refactor: ! set initial map state to have no devices

--- src/components/website/mapApp/redux/MapAppState.ts
+++ src/components/website/mapApp/redux/MapAppState.ts
@@ -37,14 +37,7 @@ export enum MapAppUsageStep {
 
 export const mapAppInitialState: MapAppState = {
     usageStep: MapAppUsageStep.HOME_SCREEN,
-    devices: [
-        { id: 'dev1', location: { lat: 42.85862508449081, lng: 74.6085298061371 } },
-        { id: 'dev2a', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-        { id: 'dev2b', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-        { id: 'dev2c', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-        { id: 'dev2d', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-        { id: 'dev3', location: { lat: 42.85610049481582, lng: 74.60671663284303 } },
-    ],
+    devices: [],
     selectedMarker: {
         location: null,
         address: null,
