3b5a0d421ec7e62864163a6ff335464f0305c1ec
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-25 13:02:06 +0600

    test: . implement ApolloTestClient

--- src/components/website/login/redux/__test__/devicesTestHelpers.ts
+++ src/components/website/login/redux/__test__/devicesTestHelpers.ts
@@ -1,7 +1,37 @@
 import { lastValueFrom, of, toArray } from 'rxjs';
+import {
+    ApolloCache,
+    ApolloClient,
+    ApolloLink,
+    ApolloQueryResult,
+    DataProxy,
+    DefaultContext,
+    DefaultOptions,
+    DocumentNode,
+    DocumentTransform,
+    FetchResult,
+    FragmentMatcher,
+    GraphQLRequest,
+    InMemoryCache,
+    MutationOptions,
+    Observable,
+    ObservableQuery,
+    OperationVariables,
+    QueryOptions,
+    Reference,
+    RefetchQueriesInclude,
+    RefetchQueriesOptions,
+    RefetchQueriesResult,
+    Resolvers,
+    SubscriptionOptions,
+    WatchQueryOptions,
+} from '@apollo/client';
+import { ExecutionResult } from 'graphql';
+import { ObjMap } from 'graphql/jsutils/ObjMap';
 import { MapAppAction } from '../../../mapApp/redux/MapAppAction';
 import { devices } from '../devices';
 import { buildStateForDevicesTest } from '../../../../../redux/__mocks__/stateBuilders';
+import { Dependency } from '../../../../../redux/store';
 
 export async function testDevicesEpic(sentAction: MapAppAction, expectedActions: MapAppAction[]) {
     const output$ = devices(of(sentAction), buildStateForDevicesTest(), {});
@@ -9,4 +39,144 @@ export async function testDevicesEpic(sentAction: MapAppAction, expectedActions:
     const receivedAction = await lastValueFrom(output$.pipe(toArray()));
 
     expect(receivedAction).toEqual(expectedActions);
+}
+
+export class ApolloTestClient implements Dependency<ApolloClient<InMemoryCache>> {
+    link: ApolloLink;
+    cache: ApolloCache<InMemoryCache>;
+    disableNetworkFetches: boolean;
+    version: string;
+    queryDeduplication: boolean;
+    defaultOptions: DefaultOptions;
+    typeDefs: string | string[] | DocumentNode | DocumentNode[];
+
+    get documentTransform(): DocumentTransform {
+        throw new Error('Method not implemented.');
+    }
+
+    stop(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    watchQuery<T = any, TVariables extends OperationVariables = OperationVariables>(
+        options: WatchQueryOptions<TVariables, T>,
+    ): ObservableQuery<T, TVariables> {
+        throw new Error('Method not implemented.');
+    }
+
+    query<T = any, TVariables extends OperationVariables = OperationVariables>(
+        options: QueryOptions<TVariables, T>,
+    ): Promise<ApolloQueryResult<T>> {
+        throw new Error('Method not implemented.');
+    }
+
+    mutate<
+        TData = any,
+        TVariables extends OperationVariables = OperationVariables,
+        TContext extends Record<string, any> = DefaultContext,
+        TCache extends ApolloCache<any> = ApolloCache<any>,
+    >(options: MutationOptions<TData, TVariables, TContext, ApolloCache<any>>): Promise<FetchResult<TData>> {
+        throw new Error('Method not implemented.');
+    }
+
+    subscribe<T = any, TVariables extends OperationVariables = OperationVariables>(
+        options: SubscriptionOptions<TVariables, T>,
+    ): Observable<FetchResult<T>> {
+        throw new Error('Method not implemented.');
+    }
+
+    readQuery<T = any, TVariables = OperationVariables>(
+        options: DataProxy.Query<TVariables, T>,
+        optimistic?: boolean,
+    ): T {
+        throw new Error('Method not implemented.');
+    }
+
+    readFragment<T = any, TVariables = OperationVariables>(
+        options: DataProxy.Fragment<TVariables, T>,
+        optimistic?: boolean,
+    ): T {
+        throw new Error('Method not implemented.');
+    }
+
+    writeQuery<TData = any, TVariables = OperationVariables>(
+        options: DataProxy.WriteQueryOptions<TData, TVariables>,
+    ): Reference {
+        throw new Error('Method not implemented.');
+    }
+
+    writeFragment<TData = any, TVariables = OperationVariables>(
+        options: DataProxy.WriteFragmentOptions<TData, TVariables>,
+    ): Reference {
+        throw new Error('Method not implemented.');
+    }
+
+    __actionHookForDevTools(cb: () => any): void {
+        throw new Error('Method not implemented.');
+    }
+
+    __requestRaw(
+        payload: GraphQLRequest<Record<string, any>>,
+    ): Observable<ExecutionResult<ObjMap<unknown>, ObjMap<unknown>>> {
+        throw new Error('Method not implemented.');
+    }
+
+    resetStore(): Promise<ApolloQueryResult<any>[]> {
+        throw new Error('Method not implemented.');
+    }
+
+    clearStore(): Promise<any[]> {
+        throw new Error('Method not implemented.');
+    }
+
+    onResetStore(cb: () => Promise<any>): () => void {
+        throw new Error('Method not implemented.');
+    }
+
+    onClearStore(cb: () => Promise<any>): () => void {
+        throw new Error('Method not implemented.');
+    }
+
+    reFetchObservableQueries(includeStandby?: boolean): Promise<ApolloQueryResult<any>[]> {
+        throw new Error('Method not implemented.');
+    }
+
+    refetchQueries<
+        TCache extends ApolloCache<any> = ApolloCache<InMemoryCache>,
+        TResult = Promise<ApolloQueryResult<any>>,
+    >(options: RefetchQueriesOptions<TCache, TResult>): RefetchQueriesResult<TResult> {
+        throw new Error('Method not implemented.');
+    }
+
+    getObservableQueries(include?: RefetchQueriesInclude): Map<string, ObservableQuery<any, OperationVariables>> {
+        throw new Error('Method not implemented.');
+    }
+
+    extract(optimistic?: boolean): InMemoryCache {
+        throw new Error('Method not implemented.');
+    }
+
+    restore(serializedState: InMemoryCache): ApolloCache<InMemoryCache> {
+        throw new Error('Method not implemented.');
+    }
+
+    addResolvers(resolvers: Resolvers | Resolvers[]): void {
+        throw new Error('Method not implemented.');
+    }
+
+    setResolvers(resolvers: Resolvers | Resolvers[]): void {
+        throw new Error('Method not implemented.');
+    }
+
+    getResolvers(): Resolvers {
+        throw new Error('Method not implemented.');
+    }
+
+    setLocalStateFragmentMatcher(fragmentMatcher: FragmentMatcher): void {
+        throw new Error('Method not implemented.');
+    }
+
+    setLink(newLink: ApolloLink): void {
+        throw new Error('Method not implemented.');
+    }
 }
