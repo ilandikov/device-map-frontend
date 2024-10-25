de73183c8e77ffdfabceb8f1bf6c6d38b1f7dc90
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-25 13:36:45 +0600

    refactor: . extract fakeQueryResponse

--- src/components/website/login/redux/devices.ts
+++ src/components/website/login/redux/devices.ts
@@ -8,18 +8,7 @@ export const devices: RootEpic = (action$) => {
     return action$.pipe(
         ofType(MapAppActionType.REMOTE_REQUEST),
         switchMap(() => {
-            return fromPromise(
-                Promise.resolve({
-                    data: [
-                        { id: 'dev1', location: { lat: 42.85862508449081, lng: 74.6085298061371 } },
-                        { id: 'dev2a', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-                        { id: 'dev2b', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-                        { id: 'dev2c', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-                        { id: 'dev2d', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
-                        { id: 'dev3', location: { lat: 42.85610049481582, lng: 74.60671663284303 } },
-                    ],
-                }),
-            ).pipe(
+            return fromPromise(fakeQueryResponse).pipe(
                 mergeMap((response) => {
                     return of(mapAppRemoteAnswer(response.data));
                 }),
@@ -27,3 +16,14 @@ export const devices: RootEpic = (action$) => {
         }),
     );
 };
+
+const fakeQueryResponse = Promise.resolve({
+    data: [
+        { id: 'dev1', location: { lat: 42.85862508449081, lng: 74.6085298061371 } },
+        { id: 'dev2a', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+        { id: 'dev2b', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+        { id: 'dev2c', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+        { id: 'dev2d', location: { lat: 42.85883742844907, lng: 74.6039915084839 } },
+        { id: 'dev3', location: { lat: 42.85610049481582, lng: 74.60671663284303 } },
+    ],
+});
