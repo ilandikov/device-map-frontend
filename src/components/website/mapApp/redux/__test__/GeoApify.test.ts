f46546dde6ca20f64ef5e66c36ec724c676181f8
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-29 23:44:53 +0600

    test: - test other cases of GeoApify epic

--- src/components/website/mapApp/redux/__test__/GeoApify.test.ts
+++ src/components/website/mapApp/redux/__test__/GeoApify.test.ts
@@ -19,6 +19,13 @@ async function testGeoApifyEpic(
 }
 
 describe('GeoApify tests', () => {
+    it('should not answer to a random action', async () => {
+        const sentAction = { type: 'RANDOM_ACTION' };
+
+        // @ts-expect-error
+        await testGeoApifyEpic(Promise.resolve({}), sentAction, []);
+    });
+
     it('should get address for a location in Bishkek', async () => {
         const remoteAnswer = Promise.resolve(chui120Response);
         const sentAction = mapAppGetLocationAddress({ lat: 42.875352500000005, lon: 74.60261920574811 });
@@ -28,6 +35,14 @@ describe('GeoApify tests', () => {
         });
 
         await testGeoApifyEpic(remoteAnswer, sentAction, [expectedAction]);
+    });
+
+    it('should show error from the remote', async () => {
+        const remoteAnswer = Promise.reject('something went wrong');
+        const sentAction = mapAppGetLocationAddress({ lat: 42.875352500000005, lon: 74.60261920574811 });
+
+        // @ts-expect-error
+        await testGeoApifyEpic(remoteAnswer, sentAction, ['something went wrong']);
     });
 });
 
