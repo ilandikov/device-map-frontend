9a1b951aff99edadd25aa4c06354b64a2a55c028
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-29 16:12:17 +0600

    test: . extract testListDevicesProcessor()

--- src/components/website/login/redux/__test__/devicesTestHelpers.ts
+++ src/components/website/login/redux/__test__/devicesTestHelpers.ts
@@ -1,7 +1,9 @@
 import { lastValueFrom, of, toArray } from 'rxjs';
-import { MapAppAction } from '../../../mapApp/redux/MapAppAction';
-import { devices } from '../devices';
+import { ApolloQueryResult } from '@apollo/client';
+import { MapAppAction, MapAppRemoteAnswer } from '../../../mapApp/redux/MapAppAction';
+import { devices, processListDevicesRequest } from '../devices';
 import { buildStateForDevicesTest } from '../../../../../redux/__mocks__/stateBuilders';
+import { T22ListDevicesResponse } from '../devicesHelpers';
 
 export async function testDevicesEpic(sentAction: MapAppAction, expectedActions: MapAppAction[]) {
     const output$ = devices(of(sentAction), buildStateForDevicesTest(), {
@@ -30,4 +32,13 @@ export async function testDevicesEpic(sentAction: MapAppAction, expectedActions:
     const receivedAction = await lastValueFrom(output$.pipe(toArray()));
 
     expect(receivedAction).toEqual(expectedActions);
+}
+
+export async function testListDevicesProcessor(
+    remoteAnswer: Promise<ApolloQueryResult<T22ListDevicesResponse>>,
+    expectedAction: MapAppRemoteAnswer,
+) {
+    const receivedAction = await lastValueFrom(processListDevicesRequest(remoteAnswer).pipe(toArray()));
+
+    expect(receivedAction).toEqual([expectedAction]);
 }
