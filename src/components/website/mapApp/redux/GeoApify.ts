2696b54fc70fa244fc071d152cbccd1acbd79828
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-10-29 22:52:15 +0600

    refactor: . convert request into function

--- src/components/website/mapApp/redux/GeoApify.ts
+++ src/components/website/mapApp/redux/GeoApify.ts
@@ -4,19 +4,23 @@ import { catchError, map, mergeMap, of } from 'rxjs';
 import { RootEpic } from '../../../../redux/store';
 import { MapAppActionType, mapAppSetLocationAddress } from './MapAppAction';
 import { GeoApifyResponse, buildMapAppAddress } from './GeoApifyHelpers';
+import { MapAppLocation } from './MapAppState';
 
 export const GeoApify: RootEpic = (action$, _state$, { cognitoClient }) =>
     action$.pipe(
         ofType(MapAppActionType.GET_LOCATION_ADDRESS),
         mergeMap((action) => {
             const location = action.location;
-            const request = ajax<GeoApifyResponse>({
-                url: `https://api.geoapify.com/v1/geocode/reverse?lat=${location.lat}&lon=${location.lon}&apiKey=8b2ff18a6cd44e7a9a916eb52cc51f8b&lang=ru`,
-                method: 'GET',
-                headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-                crossDomain: true,
-            });
-            return request.pipe(
+
+            const request = (location: MapAppLocation) =>
+                ajax<GeoApifyResponse>({
+                    url: `https://api.geoapify.com/v1/geocode/reverse?lat=${location.lat}&lon=${location.lon}&apiKey=8b2ff18a6cd44e7a9a916eb52cc51f8b&lang=ru`,
+                    method: 'GET',
+                    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+                    crossDomain: true,
+                });
+
+            return request(location).pipe(
                 map((ajaxResponse) => {
                     const geoApifyAddress = buildMapAppAddress(ajaxResponse.response);
                     return mapAppSetLocationAddress(geoApifyAddress);
