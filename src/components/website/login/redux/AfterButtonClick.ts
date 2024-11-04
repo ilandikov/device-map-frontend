b9c223c388e5f1b6095e4d12b42cff02dc034ed9
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-05 04:12:57 +0600

    refactor: - replace switch with map

--- src/components/website/login/redux/AfterButtonClick.ts
+++ src/components/website/login/redux/AfterButtonClick.ts
@@ -6,36 +6,30 @@ export function afterButtonClick(
     action: LoginModalButtonClick,
     state: AuthenticationState,
 ): Partial<AuthenticationState> {
-    switch (action.button) {
-        case LoginModalButton.ACCOUNT_REGISTER:
-            return { step: AuthenticationStep.MAIL_INPUT };
-        case LoginModalButton.ACCOUNT_LOGIN:
-            return { step: AuthenticationStep.LOGIN, error: null };
-        case LoginModalButton.CANCEL:
-            return initialAuthenticationState;
-        case LoginModalButton.RESET_PASSWORD:
-            return {
-                step: AuthenticationStep.PASSWORD_RESET_REQUEST,
-                password: '',
-                error: null,
-            };
-        case LoginModalButton.USER_BUTTON:
-            return initialAuthenticationState;
-        // TODO rename this to start auth
-        case LoginModalButton.NEXT:
-            switch (state.step) {
-                case AuthenticationStep.PASSWORD_RESET_OTP:
-                    return { step: AuthenticationStep.PASSWORD_RESET_LOADING };
-                // TODO remove this case and simplify to one line
-                default:
-                    return {};
-            }
-        case LoginModalButton.GO_BACK:
-            return { step: goBackFrom[state.step] };
-    }
-
-    return {};
+    const stateTransformer = stateTransformers[action.button];
+    return stateTransformer(state);
 }
+
+type StateTransformer = (state: AuthenticationState) => Partial<AuthenticationState>;
+
+const stateTransformers: { [key in LoginModalButton]: StateTransformer } = {
+    ACCOUNT_REGISTER: () => ({ step: AuthenticationStep.MAIL_INPUT }),
+    ACCOUNT_LOGIN: () => ({ step: AuthenticationStep.LOGIN, error: null }),
+    CANCEL: () => ({ ...initialAuthenticationState }),
+    RESET_PASSWORD: () => ({ step: AuthenticationStep.PASSWORD_RESET_REQUEST, password: '', error: null }),
+    USER_BUTTON: () => ({ ...initialAuthenticationState }),
+    // TODO rename this to start auth
+    NEXT: (state) => {
+        switch (state.step) {
+            case AuthenticationStep.PASSWORD_RESET_OTP:
+                return { step: AuthenticationStep.PASSWORD_RESET_LOADING };
+            // TODO remove this case and simplify to one line
+            default:
+                return {};
+        }
+    },
+    GO_BACK: (state) => ({ step: goBackFrom[state.step] }),
+};
 
 const goBackFrom: StepMap = {
     MAIL_INPUT: AuthenticationStep.WELCOME,
