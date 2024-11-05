c073367a50e69d2a1c077f2cb774fdac3b0d10fc
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-05 22:40:31 +0600

    refactor: - remove now unused field

--- src/components/website/login/redux/cognito.ts
+++ src/components/website/login/redux/cognito.ts
@@ -30,7 +30,6 @@ export const cognito: RootEpic = (action$, state$, { cognitoClient }) =>
 type AuthenticationMethod = {
     call: (cognitoClient: Dependency<CognitoClient>, authenticationState: AuthenticationState) => Promise<any>;
     answerType: LoginModalRemoteAnswerType;
-    availableStep: AuthenticationStep;
     completesAuthentication?: boolean;
 };
 
@@ -60,31 +59,26 @@ const authenticationMethods: Partial<{ [key in AuthenticationStep]: Authenticati
         call: (cognitoClient, authenticationState) =>
             cognitoClient.signUp(authenticationState.email, authenticationState.password),
         answerType: LoginModalRemoteAnswerType.SIGN_UP,
-        availableStep: AuthenticationStep.PASSWORD_CREATION_LOADING,
     },
     PASSWORD_CREATION_OTP_LOADING: {
         call: (cognitoClient, authenticationState) =>
             cognitoClient.signUpConfirmCode(authenticationState.email, authenticationState.OTP),
         answerType: LoginModalRemoteAnswerType.OTP,
-        availableStep: AuthenticationStep.PASSWORD_CREATION_OTP_LOADING,
         completesAuthentication: true,
     },
     PASSWORD_CREATION_OTP_RESEND_LOADING: {
         call: (cognitoClient, authenticationState) => cognitoClient.resendConfirmCode(authenticationState.email),
         answerType: LoginModalRemoteAnswerType.OTP_RESEND,
-        availableStep: AuthenticationStep.PASSWORD_CREATION_OTP_RESEND_LOADING,
     },
     LOGIN_LOADING: {
         call: (cognitoClient, authenticationState) =>
             cognitoClient.signIn(authenticationState.email, authenticationState.password),
         answerType: LoginModalRemoteAnswerType.SIGN_IN,
-        availableStep: AuthenticationStep.LOGIN_LOADING,
         completesAuthentication: true,
     },
     PASSWORD_RESET_REQUEST_LOADING: {
         call: (cognitoClient, authenticationState) => cognitoClient.forgotPassword(authenticationState.email),
         answerType: LoginModalRemoteAnswerType.FORGOT_PASSWORD,
-        availableStep: AuthenticationStep.PASSWORD_RESET_REQUEST_LOADING,
     },
     PASSWORD_RESET_LOADING: {
         call: (cognitoClient, authenticationState) =>
@@ -94,12 +88,10 @@ const authenticationMethods: Partial<{ [key in AuthenticationStep]: Authenticati
                 authenticationState.password,
             ),
         answerType: LoginModalRemoteAnswerType.PASSWORD_RESET,
-        availableStep: AuthenticationStep.PASSWORD_RESET_LOADING,
         completesAuthentication: true,
     },
     LOGGED_IN: {
         call: (cognitoClient, _) => cognitoClient.signOut(),
-        availableStep: AuthenticationStep.LOGGED_IN,
         answerType: LoginModalRemoteAnswerType.SIGN_OUT,
     },
 };
