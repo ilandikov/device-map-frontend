38d29009be9736dd0b730d5027654ed35f3ae5dc
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-05 18:47:08 +0600

    refactor: - remove switch statement

--- src/components/website/login/redux/cognito.ts
+++ src/components/website/login/redux/cognito.ts
@@ -1,7 +1,7 @@
 import { EMPTY, switchMap } from 'rxjs';
 import { ofType } from 'redux-observable';
 import { RootEpic } from '../../../../redux/store';
-import { LoginModalActionType, LoginModalRemoteRequestType } from './LoginModalAction';
+import { LoginModalActionType } from './LoginModalAction';
 import { authenticationMethods, processAuthMethod } from './cognitoEndpoints';
 
 export const cognito: RootEpic = (action$, state$, { cognitoClient }) => {
@@ -18,46 +18,11 @@ export const cognito: RootEpic = (action$, state$, { cognitoClient }) => {
                 return EMPTY;
             }
 
-            switch (action.request) {
-                case LoginModalRemoteRequestType.PASSWORD:
-                    return processAuthMethod(
-                        `${action.request}.${authenticationState.step}`,
-                        cognitoClient,
-                        authenticationState,
-                    );
-                case LoginModalRemoteRequestType.USERNAME_AND_PASSWORD:
-                    return processAuthMethod(
-                        `${action.request}.${authenticationState.step}`,
-                        cognitoClient,
-                        authenticationState,
-                    );
-                case LoginModalRemoteRequestType.OTP:
-                    return processAuthMethod(
-                        `${action.request}.${authenticationState.step}`,
-                        cognitoClient,
-                        authenticationState,
-                    );
-                case LoginModalRemoteRequestType.OTP_RESEND:
-                    return processAuthMethod(
-                        `${action.request}.${authenticationState.step}`,
-                        cognitoClient,
-                        authenticationState,
-                    );
-                case LoginModalRemoteRequestType.USERNAME:
-                    return processAuthMethod(
-                        `${action.request}.${authenticationState.step}`,
-                        cognitoClient,
-                        authenticationState,
-                    );
-                case LoginModalRemoteRequestType.SIGN_OUT:
-                    return processAuthMethod(
-                        `${action.request}.${authenticationState.step}`,
-                        cognitoClient,
-                        authenticationState,
-                    );
-                default:
-                    return EMPTY;
-            }
+            return processAuthMethod(
+                `${action.request}.${authenticationState.step}`,
+                cognitoClient,
+                authenticationState,
+            );
         }),
     );
 };
