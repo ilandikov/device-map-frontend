112299d8dfb9afe639a2618b08eee02a972995bd
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-11 20:36:14 +0600

    test: - remove unused parameter and variables

--- src/components/website/login/redux/__test__/devices.test.ts
+++ src/components/website/login/redux/__test__/devices.test.ts
@@ -5,7 +5,6 @@ import {
     mapAppRemoteRequest,
     mapAppSetDevices,
 } from '../../../mapApp/redux/MapAppAction';
-import { buildMapAppState } from '../../../mapApp/redux/MapAppState';
 import { DevicesClient } from '../../../../../redux/store';
 import { testDevicesEpic } from './devicesTestHelpers';
 
@@ -32,53 +31,43 @@ const rejectingClient: DevicesClient = {
 
 describe('devices epic test', () => {
     it('should return no action to a non-remote request action', async () => {
-        const mapAppState = buildMapAppState({});
         const sentAction = { type: 'notAMapAppAction' };
         const expectedActions = [];
 
         // @ts-expect-error
-        await testDevicesEpic(mapAppState, resolvingClient, sentAction, expectedActions);
+        await testDevicesEpic(resolvingClient, sentAction, expectedActions);
     });
 });
 
 describe('list devices', () => {
     it('should process a resolved promise', async () => {
-        const mapAppState = buildMapAppState({});
         const sentAction = mapAppRemoteRequest(MapAppRemoteRequestType.LIST_DEVICES);
         const expectedAction = mapAppSetDevices([
             { id: 'dev1', location: { lat: 42.85862508449081, lon: 74.6085298061371 } },
         ]);
 
-        await testDevicesEpic(mapAppState, resolvingClient, sentAction, [expectedAction]);
+        await testDevicesEpic(resolvingClient, sentAction, [expectedAction]);
     });
 
     it('should process a rejected promise', async () => {
-        const mapAppState = buildMapAppState({});
         const sentAction = mapAppRemoteRequest(MapAppRemoteRequestType.LIST_DEVICES);
 
-        await testDevicesEpic(mapAppState, rejectingClient, sentAction, []);
+        await testDevicesEpic(rejectingClient, sentAction, []);
     });
 });
 
 describe('devices - create device', () => {
     it('should send action with the new device', async () => {
-        const mapAppState = buildMapAppState({
-            selectedMarker: {
-                location: { lat: 2, lon: 3 },
-                address: null,
-            },
-        });
         const sentAction = mapAppRemoteRequest(MapAppRemoteRequestType.CREATE_DEVICE);
         const expectedAction = mapAppAddDevice({ id: 'testId', location: { lat: 2, lon: 3 } });
 
-        await testDevicesEpic(mapAppState, resolvingClient, sentAction, [expectedAction]);
+        await testDevicesEpic(resolvingClient, sentAction, [expectedAction]);
     });
 
     it.failing('should notify about the error', async () => {
-        const mapAppState = buildMapAppState({});
         const sentAction = mapAppRemoteRequest(MapAppRemoteRequestType.CREATE_DEVICE);
         const expectedAction = mapAppRemoteErrorAnswer('create device went wrong');
 
-        await testDevicesEpic(mapAppState, rejectingClient, sentAction, [expectedAction]);
+        await testDevicesEpic(rejectingClient, sentAction, [expectedAction]);
     });
 });
--- src/components/website/login/redux/__test__/devicesTestHelpers.ts
+++ src/components/website/login/redux/__test__/devicesTestHelpers.ts
@@ -2,11 +2,10 @@ import { lastValueFrom, of, toArray } from 'rxjs';
 import { MapAppAction } from '../../../mapApp/redux/MapAppAction';
 import { devices } from '../devices';
 import { buildStateForDevicesTest } from '../../../../../redux/__mocks__/stateBuilders';
-import { MapAppState, buildMapAppState } from '../../../mapApp/redux/MapAppState';
+import { buildMapAppState } from '../../../mapApp/redux/MapAppState';
 import { DevicesClient } from '../../../../../redux/store';
 
 export async function testDevicesEpic(
-    _mapAppState: MapAppState,
     devicesClient: DevicesClient,
     sentAction: MapAppAction,
     expectedActions: MapAppAction[],
