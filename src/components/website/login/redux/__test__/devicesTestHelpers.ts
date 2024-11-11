4eed3eed7fbe6ec712ac92e802d81122ed42fb64
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-11 20:35:19 +0600

    test: - set initial state in the testing function

--- src/components/website/login/redux/__test__/devicesTestHelpers.ts
+++ src/components/website/login/redux/__test__/devicesTestHelpers.ts
@@ -2,16 +2,16 @@ import { lastValueFrom, of, toArray } from 'rxjs';
 import { MapAppAction } from '../../../mapApp/redux/MapAppAction';
 import { devices } from '../devices';
 import { buildStateForDevicesTest } from '../../../../../redux/__mocks__/stateBuilders';
-import { MapAppState } from '../../../mapApp/redux/MapAppState';
+import { MapAppState, buildMapAppState } from '../../../mapApp/redux/MapAppState';
 import { DevicesClient } from '../../../../../redux/store';
 
 export async function testDevicesEpic(
-    mapAppState: MapAppState,
+    _mapAppState: MapAppState,
     devicesClient: DevicesClient,
     sentAction: MapAppAction,
     expectedActions: MapAppAction[],
 ) {
-    const output$ = devices(of(sentAction), buildStateForDevicesTest(mapAppState), {
+    const output$ = devices(of(sentAction), buildStateForDevicesTest(buildMapAppState({})), {
         devicesClient: devicesClient,
     });
 
