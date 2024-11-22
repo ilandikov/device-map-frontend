0afd8daf4a01ebb8c7256cf8b9fc5fd4e3406d5b
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-23 01:37:44 +0600

    test: - test DeviceListItem rendering depending on user log in

--- src/components/website/mapApp/__test__/DeviceListItem.test.tsx
+++ src/components/website/mapApp/__test__/DeviceListItem.test.tsx
@@ -1,15 +1,40 @@
 import { fireEvent, getByTestId } from '@testing-library/react';
 import React from 'react';
-import { mockDispatch, mockPrepareSelector } from '../../../../redux/__mocks__/mocks';
-import { renderForActionDispatchTest } from '../../../../../tests/utils/RenderingHelpers';
+import { mockAuthenticationState, mockDispatch, mockPrepareSelector } from '../../../../redux/__mocks__/mocks';
+import { renderForActionDispatchTest, renderForSnapshotTest } from '../../../../../tests/utils/RenderingHelpers';
 import { mapAppDeleteDeviceRequest } from '../redux/MapAppAction';
 import { DeviceListItem } from '../DeviceListItem';
+import { AuthenticationStep } from '../../login/redux/AuthenticationState';
 
 jest.mock('react-redux', () => ({
     ...jest.requireActual('react-redux'),
     useDispatch: () => mockDispatch,
     useSelector: () => mockPrepareSelector(),
 }));
+
+describe('DeviceListItem snapshot test', () => {
+    it('should match snapshot when user is not authenticated', () => {
+        mockAuthenticationState({});
+        const component = renderForSnapshotTest(
+            <DeviceListItem
+                device={{ id: 'i should render without delete button', location: { lat: 3, lon: 7 } }}
+                colorIndex={0}
+            />,
+        );
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should match snapshot with error', () => {
+        mockAuthenticationState({ step: AuthenticationStep.LOGGED_IN });
+        const component = renderForSnapshotTest(
+            <DeviceListItem
+                device={{ id: 'i should render with delete button', location: { lat: 3, lon: 7 } }}
+                colorIndex={0}
+            />,
+        );
+        expect(component).toMatchSnapshot();
+    });
+});
 
 describe('DeviceListItem action tests', () => {
     beforeEach(() => {
