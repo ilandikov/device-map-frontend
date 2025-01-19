1a713f6098c8c6c2105aa0090706d911cc9384e6
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-01-19 11:29:11 +0600

    refactor: . move component to separate file

---
+++ src/components/website/mapApp/ApproveButton.tsx
@@ -0,0 +1,19 @@
+import { useI18next } from 'gatsby-plugin-react-i18next';
+import React from 'react';
+import { useAppDispatch } from '../../../redux/store';
+import { mapAppApproveDeviceRequest } from './redux/MapAppAction';
+
+export function ApproveButton(props: { id: string }) {
+    const { t } = useI18next();
+    const dispatch = useAppDispatch();
+
+    return (
+        <button
+            className="device-list-item-opaque-text"
+            data-testid="approveDeviceButton"
+            onClick={() => dispatch(mapAppApproveDeviceRequest(props.id))}
+        >
+            {t('approveDevice')}
+        </button>
+    );
+}
--- src/components/website/mapApp/DeviceListItem.tsx
+++ src/components/website/mapApp/DeviceListItem.tsx
@@ -1,25 +1,9 @@
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 import { T22Device } from '@mancho-school-t22/graphql-types';
-import { useAppDispatch } from '../../../redux/store';
 import { DeviceListItemWrapper, getDeviceApprovalStatus } from './DeviceListItemWrapper';
 import { DeleteButton } from './DeleteButton';
-import { mapAppApproveDeviceRequest } from './redux/MapAppAction';
-
-function ApproveButton(props: { id: string }) {
-    const { t } = useI18next();
-    const dispatch = useAppDispatch();
-
-    return (
-        <button
-            className="device-list-item-opaque-text"
-            data-testid="approveDeviceButton"
-            onClick={() => dispatch(mapAppApproveDeviceRequest(props.id))}
-        >
-            {t('approveDevice')}
-        </button>
-    );
-}
+import { ApproveButton } from './ApproveButton';
 
 export function DeviceListItem(props: { device: T22Device; approvals: number; createdByCurrentUser: boolean }) {
     const { t } = useI18next();
