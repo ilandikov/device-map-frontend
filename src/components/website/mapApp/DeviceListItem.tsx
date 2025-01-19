3c19b2a7b39ab2e92a06102a4cab6197aa2101a3
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-01-19 11:28:29 +0600

    refactor: - extract ApproveButton() component

--- src/components/website/mapApp/DeviceListItem.tsx
+++ src/components/website/mapApp/DeviceListItem.tsx
@@ -3,12 +3,26 @@ import React from 'react';
 import { T22Device } from '@mancho-school-t22/graphql-types';
 import { useAppDispatch } from '../../../redux/store';
 import { DeviceListItemWrapper, getDeviceApprovalStatus } from './DeviceListItemWrapper';
-import { mapAppApproveDeviceRequest } from './redux/MapAppAction';
 import { DeleteButton } from './DeleteButton';
+import { mapAppApproveDeviceRequest } from './redux/MapAppAction';
+
+function ApproveButton(props: { id: string }) {
+    const { t } = useI18next();
+    const dispatch = useAppDispatch();
+
+    return (
+        <button
+            className="device-list-item-opaque-text"
+            data-testid="approveDeviceButton"
+            onClick={() => dispatch(mapAppApproveDeviceRequest(props.id))}
+        >
+            {t('approveDevice')}
+        </button>
+    );
+}
 
 export function DeviceListItem(props: { device: T22Device; approvals: number; createdByCurrentUser: boolean }) {
     const { t } = useI18next();
-    const dispatch = useAppDispatch();
 
     const canDeviceBeApproved =
         getDeviceApprovalStatus(props.approvals) === 'created' ||
@@ -19,15 +33,7 @@ export function DeviceListItem(props: { device: T22Device; approvals: number; cr
             <p>{props.device.id}</p>
             <button className="device-list-item-opaque-text">{t('deviceReportBroken')}</button>
             {props.createdByCurrentUser && <DeleteButton id={props.device.id} />}
-            {!props.createdByCurrentUser && canDeviceBeApproved && (
-                <button
-                    className="device-list-item-opaque-text"
-                    data-testid="approveDeviceButton"
-                    onClick={() => dispatch(mapAppApproveDeviceRequest(props.device.id))}
-                >
-                    {t('approveDevice')}
-                </button>
-            )}
+            {!props.createdByCurrentUser && canDeviceBeApproved && <ApproveButton id={props.device.id} />}
         </DeviceListItemWrapper>
     );
 }
