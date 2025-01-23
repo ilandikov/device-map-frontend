308eaef81eebba138e1e77009ae109c6ad5d2d47
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-01-23 12:28:02 +0600

    refactor: . extract type and function to separate file

--- src/components/website/mapApp/DeviceListItem.tsx
+++ src/components/website/mapApp/DeviceListItem.tsx
@@ -1,10 +1,11 @@
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 import { T22Device } from '@mancho-school-t22/graphql-types';
-import { DeviceListItemContainer, getDeviceItemType } from './DeviceListItemContainer';
+import { DeviceListItemContainer } from './DeviceListItemContainer';
 import { DeleteButton } from './DeleteButton';
 import { ApproveButton } from './ApproveButton';
 import { MapAppUsageStep, useMapAppState } from './redux/MapAppState';
+import { getDeviceItemType } from './DeviceItemType';
 
 export function DeviceListItem(props: { device: T22Device; createdByCurrentUser: boolean }) {
     const { t } = useI18next();
--- src/components/website/mapApp/DeviceListItemContainer.tsx
+++ src/components/website/mapApp/DeviceListItemContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import terminals from '../../../assets/images/Terminals.png';
+import { DeviceItemType } from './DeviceItemType';
 
 export function DeviceListItemContainer(props: { deviceApprovalStatus: DeviceItemType; children: React.ReactNode }) {
     return (
@@ -14,21 +15,4 @@ export function DeviceListItemContainer(props: { deviceApprovalStatus: DeviceIte
             <div className="device-list-item">{props.children}</div>
         </div>
     );
-}
-
-type DeviceItemType = 'create' | 'created' | 'approving' | 'approved';
-
-export function getDeviceItemType(approvals: number): DeviceItemType {
-    switch (true) {
-        case approvals < 0:
-            return 'create';
-        case approvals === 0:
-            return 'created';
-        case approvals === 1:
-            return 'approving';
-        case approvals >= 2:
-            return 'approved';
-        default:
-            throw new Error(`Unknown approval count: ${approvals}`);
-    }
 }
