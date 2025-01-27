0146dd0f9a7ccf21575d1e0d4b6ab2bab500c290
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-01-27 14:01:07 +0600

    refactor: . rename variable

--- src/components/website/mapApp/UserPoints.tsx
+++ src/components/website/mapApp/UserPoints.tsx
@@ -7,15 +7,15 @@ import { useMapAppState } from './redux/MapAppState';
 export function UserPoints(props: { className: string }) {
     const { t } = useI18next();
 
-    const userPoints = useMapAppState().currentUserPoints;
+    const currentUserPoints = useMapAppState().currentUserPoints;
 
     return (
         <div className={`${props.className} user-points-header-block`}>
             <img src={Points} alt="user-points-image" />
-            {userPoints ? (
+            {currentUserPoints ? (
                 <>
-                    <span className="user-points">{userPoints}</span>
-                    <span className="user-points">{userPoints > 1 ? t('points') : t('point')}</span>
+                    <span className="user-points">{currentUserPoints}</span>
+                    <span className="user-points">{currentUserPoints > 1 ? t('points') : t('point')}</span>
                 </>
             ) : (
                 <div className="user-points-loader-container">
