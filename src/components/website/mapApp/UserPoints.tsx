d6723463ec83610297e5846bb51b18c465898781
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-01-27 13:56:36 +0600

    refactor: ! add loader to be shown when waiting for user points

--- src/components/website/mapApp/UserPoints.tsx
+++ src/components/website/mapApp/UserPoints.tsx
@@ -1,6 +1,7 @@
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 import Points from '/src/assets/images/Points.svg';
+import { FinikLoader } from '../common/FinikLoader';
 
 export function UserPoints(props: { className: string }) {
     const { t } = useI18next();
@@ -9,8 +10,16 @@ export function UserPoints(props: { className: string }) {
     return (
         <div className={`${props.className} user-points-header-block`}>
             <img src={Points} alt="user-points-image" />
-            <span className="user-points">{userPoints}</span>
-            <span className="user-points">{userPoints > 1 ? t('points') : t('point')}</span>
+            {userPoints !== null ? (
+                <>
+                    <span className="user-points">{userPoints}</span>
+                    <span className="user-points">{userPoints > 1 ? t('points') : t('point')}</span>
+                </>
+            ) : (
+                <div className="user-points-loader-container">
+                    <FinikLoader />
+                </div>
+            )}
         </div>
     );
 }
