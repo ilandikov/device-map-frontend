20c1e6d798e86f2ef2d58125c043678e9084af59
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-10 22:17:04 +0600

    test: - remove unneeded data for the test

--- src/components/website/mapApp/__test__/MapApp.test.tsx
+++ src/components/website/mapApp/__test__/MapApp.test.tsx
@@ -32,7 +32,6 @@ describe('MapApp snapshot tests', () => {
     it('should match the snapshot at device management state', () => {
         mockMapAppState({
             usageStep: MapAppUsageStep.DEVICE_MANAGEMENT,
-            currentUserID: 'logged in',
         });
         const component = renderForSnapshotTest(<MapApp />);
 
