28cf044d07292e67612688d7881bbce7c70d92e4
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-11 18:28:18 +0600

    refactor: . renames

--- src/components/website/mapApp/redux/MapAppReducer.ts
+++ src/components/website/mapApp/redux/MapAppReducer.ts
@@ -31,8 +31,8 @@ export function MapAppReducer(state: MapAppState = mapAppInitialState, action: M
             return { ...state, currentUserPoints: null };
         case MapAppActionType.SET_USER_POINTS:
             return { ...state, currentUserPoints: action.points };
-        case MapAppActionType.SET_USAGE_STEP:
-            return { ...state, component: action.step };
+        case MapAppActionType.SHOW_COMPONENT:
+            return { ...state, component: action.component };
         case DeviceActionType.DEVICE_REMOTE_ANSWER:
             return { ...state, devices: deviceReducer(state.devices, action) };
         default:
