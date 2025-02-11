93f641601224843322c93a7a3594800700982e90
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-11 16:45:23 +0500

    refactor: - simplify reducer

--- src/components/website/mapApp/redux/AfterButtonClicked.ts
+++ src/components/website/mapApp/redux/AfterButtonClicked.ts
@@ -1,11 +1,7 @@
-import { MapAppButton, MapAppButtonClick } from './MapAppAction';
+import { MapAppButtonClick } from './MapAppAction';
 import { MapAppState } from './MapAppState';
 
 // TODO these actions should reset the user and that's all. Setting usage step to be done with a separate action
-export function afterButtonClicked(action: MapAppButtonClick): Partial<MapAppState> {
-    if (action.button === MapAppButton.LOGOUT) {
-        return { currentUserID: null };
-    }
-
-    return {};
+export function afterButtonClicked(_action: MapAppButtonClick): Partial<MapAppState> {
+    return { currentUserID: null };
 }
