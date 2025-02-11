3e0958410f94dadc8917a7298af544f10e42e3c5
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-11 16:10:27 +0500

    refactor: - remove useless condition

--- src/components/website/mapApp/redux/AfterButtonClicked.ts
+++ src/components/website/mapApp/redux/AfterButtonClicked.ts
@@ -1,19 +1,11 @@
 import { MapAppButton, MapAppButtonClick } from './MapAppAction';
-import { MapAppState, MapAppUsageStep } from './MapAppState';
+import { MapAppState } from './MapAppState';
 
 // TODO these actions should reset the user and that's all. Setting usage step to be done with a separate action
 export function afterButtonClicked(action: MapAppButtonClick): Partial<MapAppState> {
-    const nextStep = stepAfterButtonClick[action.button];
-    if (nextStep) {
-        if (action.button === MapAppButton.LOGOUT) {
-            return { currentUserID: null };
-        }
+    if (action.button === MapAppButton.LOGOUT) {
+        return { currentUserID: null };
     }
 
     return {};
 }
-
-const stepAfterButtonClick: { [key in MapAppButton]: MapAppUsageStep } = {
-    LOGIN: MapAppUsageStep.USER_AUTHENTICATION,
-    LOGOUT: MapAppUsageStep.HOME_SCREEN,
-};
