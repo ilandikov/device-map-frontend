ba863570452f3cc4063d14ffa23ac78476211537
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-14 16:19:11 +0600

    test: - remove excessive state builder calls

--- src/components/website/login/redux/__test__/Authentication.test.ts
+++ src/components/website/login/redux/__test__/Authentication.test.ts
@@ -51,8 +51,7 @@ describe('LoginModal reducer tests', () => {
     });
 
     it('any input shall reset the error', () => {
-        const initialState = buildAuthenticationState({ error: new Error('shouldDisappear') });
-        // @ts-expect-error
+        const initialState = { error: new Error('shouldDisappear') }; // @ts-expect-error
         const action = loginModalInput('wrongType', '');
 
         testStateChange(initialState, action, {
@@ -63,7 +62,7 @@ describe('LoginModal reducer tests', () => {
 
 describe('welcome screen buttons', () => {
     it('should transition to email input', () => {
-        const initialState = buildAuthenticationState({ step: AuthenticationStep.WELCOME });
+        const initialState = { step: AuthenticationStep.WELCOME };
         const action = loginModalButtonClick(LoginModalButton.ACCOUNT_REGISTER);
 
         testStateChange(initialState, action, {
@@ -72,7 +71,7 @@ describe('welcome screen buttons', () => {
     });
 
     it('should transition to user login', () => {
-        const initialState = buildAuthenticationState({ step: AuthenticationStep.WELCOME });
+        const initialState = { step: AuthenticationStep.WELCOME };
         const action = loginModalButtonClick(LoginModalButton.ACCOUNT_LOGIN);
 
         testStateChange(initialState, action, {
@@ -83,14 +82,14 @@ describe('welcome screen buttons', () => {
 
 describe('navigation logic', () => {
     it('cancel button: should reset the state back to initial', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_OTP,
             email: 'something@somewhere.com',
             error: new Error('whack'),
             password: 'authMePls',
             passwordRepeat: 'authMePls',
             OTP: '654342',
-        });
+        };
         const action = loginModalButtonClick(LoginModalButton.CANCEL);
 
         testStateChange(initialState, action, {
@@ -116,7 +115,7 @@ describe('navigation logic', () => {
         // From password creation to mail input
         [AuthenticationStep.PASSWORD_CREATION, AuthenticationStep.MAIL_INPUT],
     ])('go back button: should transition from %s to %s', (initialStep, stepAfterGoBack) => {
-        const initialState = buildAuthenticationState({ step: initialStep });
+        const initialState = { step: initialStep };
         const action = loginModalButtonClick(LoginModalButton.GO_BACK);
 
         testStateChange(initialState, action, {
@@ -127,7 +126,7 @@ describe('navigation logic', () => {
 
 describe('email input logic', () => {
     it('should update user email', () => {
-        const initialState = buildAuthenticationState({ step: AuthenticationStep.MAIL_INPUT });
+        const initialState = { step: AuthenticationStep.MAIL_INPUT };
         const action = loginModalInput(LoginModalInputType.EMAIL, 'myMail@myServer.xyz');
 
         testStateChange(initialState, action, {
@@ -136,11 +135,11 @@ describe('email input logic', () => {
     });
 
     it('should remove mail error and transition to password creation after good mail has been sent to verification', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.MAIL_INPUT,
             email: 'good@email.com',
             error: new Error('omgSomethingIsWrong'),
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.USERNAME);
 
         testStateChange(initialState, action, {
@@ -150,21 +149,21 @@ describe('email input logic', () => {
     });
 
     it('should set mail error and stay at mail input when bad mail has been sent to verification', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.MAIL_INPUT,
             email: 'this is not an email!',
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.USERNAME);
 
         testStateChange(initialState, action, { error: new Error(MailInputError.NOT_VALID) });
     });
 
     it('should remove mail error and transition to login with an existing mail', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.MAIL_INPUT,
             email: 'already@exists.com',
             error: new Error(CognitoErrors.USERNAME_EXISTS),
-        });
+        };
         const action = loginModalButtonClick(LoginModalButton.ACCOUNT_LOGIN);
 
         testStateChange(initialState, action, {
@@ -176,8 +175,7 @@ describe('email input logic', () => {
 
 describe('user password logic', () => {
     it('should set user password', () => {
-        const initialState = buildAuthenticationState({});
-
+        const initialState = {};
         const action = loginModalInput(LoginModalInputType.PASSWORD, 'haha!!11');
 
         testStateChange(initialState, action, {
@@ -186,8 +184,7 @@ describe('user password logic', () => {
     });
 
     it('should set user password repeat', () => {
-        const initialState = buildAuthenticationState({});
-
+        const initialState = {};
         const action = loginModalInput(LoginModalInputType.PASSWORD_REPEAT, 'lmao!rofl!');
 
         testStateChange(initialState, action, {
@@ -196,13 +193,12 @@ describe('user password logic', () => {
     });
 
     it('should transition to loading step after password creation if passwords are matching and remove password error', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION,
             password: 'passwordsMatchAndAreStrong9%',
             passwordRepeat: 'passwordsMatchAndAreStrong9%',
             error: new Error('thisIsSoWrong'),
-        });
-
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.PASSWORD);
 
         testStateChange(initialState, action, {
@@ -212,13 +208,12 @@ describe('user password logic', () => {
     });
 
     it('should transition to loading after password reset if passwords are matching and remove password error', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET,
             password: '1!veryGoodPassword',
             passwordRepeat: '1!veryGoodPassword',
             error: new Error('shouldNotHappen'),
-        });
-
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.PASSWORD);
 
         testStateChange(initialState, action, {
@@ -228,11 +223,10 @@ describe('user password logic', () => {
     });
 
     it('should set password error if passwords are not matching', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             password: 'dontMatch',
             passwordRepeat: 'likeForSureDontMatch',
-        });
-
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.PASSWORD);
 
         testStateChange(initialState, action, {
@@ -243,9 +237,9 @@ describe('user password logic', () => {
 
 describe('sign up logic', () => {
     it('should transition to OTP input if sign up succeeded', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerSuccess();
 
         testStateChange(initialState, action, {
@@ -254,10 +248,10 @@ describe('sign up logic', () => {
     });
 
     it('should transition to mail input if sign up failed', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_LOADING,
             error: null,
-        });
+        };
         const action = loginModalRemoteAnswerFailure('thisIsWhy');
 
         testStateChange(initialState, action, {
@@ -269,11 +263,11 @@ describe('sign up logic', () => {
 
 describe('sign up OTP logic', () => {
     it('should move from sign up OTP to sign up OTP loading stage and reset the error', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_OTP,
             OTP: '451035',
             error: new Error('something is wrong with OTP'),
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.OTP);
 
         testStateChange(initialState, action, {
@@ -283,11 +277,11 @@ describe('sign up OTP logic', () => {
     });
 
     it('should move from password reset OTP to new password input step and reset the error', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_OTP,
             OTP: '781340',
             error: new Error('verifyTheCodeAgain'),
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.OTP);
 
         testStateChange(initialState, action, {
@@ -297,9 +291,9 @@ describe('sign up OTP logic', () => {
     });
 
     it('should set OTP value in the state', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_OTP,
-        });
+        };
         const action = loginModalInput(LoginModalInputType.OTP, '9832');
 
         testStateChange(initialState, action, {
@@ -308,9 +302,9 @@ describe('sign up OTP logic', () => {
     });
 
     it('should set error if OTP is less than 6 characters', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             OTP: '51094',
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.OTP);
 
         testStateChange(initialState, action, {
@@ -319,9 +313,9 @@ describe('sign up OTP logic', () => {
     });
 
     it('should transition to login after sign up has been confirmed', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_OTP_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerSuccess();
 
         testStateChange(initialState, action, {
@@ -330,9 +324,9 @@ describe('sign up OTP logic', () => {
     });
 
     it('should transition back to OTP input if a sign up confirmation failed and set an error', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_OTP_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerFailure('signUpWasNotConfirmedUnfortunately');
 
         testStateChange(initialState, action, {
@@ -344,10 +338,10 @@ describe('sign up OTP logic', () => {
 
 describe('login logic', () => {
     it('should transition to loading state on user and password verify request, reset error', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.LOGIN,
             error: new Error('userNameAndPasswordAreWrong!'),
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.NONE);
 
         testStateChange(initialState, action, {
@@ -357,12 +351,12 @@ describe('login logic', () => {
     });
 
     it('should transition from login to password reset state on password reset button click, keep the mail, reset the password and the error', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.LOGIN,
             email: 'writeMe@mail.com',
             password: 'iForgot',
             error: new Error('triedToInputPasswordButFailed'),
-        });
+        };
         const action = loginModalButtonClick(LoginModalButton.RESET_PASSWORD);
 
         testStateChange(initialState, action, {
@@ -373,9 +367,9 @@ describe('login logic', () => {
     });
 
     it('should transition from loading state to logged in on login success notification', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.LOGIN_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerSuccess();
 
         testStateChange(initialState, action, {
@@ -384,9 +378,9 @@ describe('login logic', () => {
     });
 
     it('should transition from loading state back to login step on login failure notification', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.LOGIN_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerFailure('thereHasBeenAnError');
 
         testStateChange(initialState, action, {
@@ -398,11 +392,11 @@ describe('login logic', () => {
 
 describe('password reset logic', () => {
     it('should transition to loading step and reset error on mail verify request with a valid email', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_REQUEST,
             email: 'valid@mail.com',
             error: new Error(MailInputError.NOT_VALID),
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.USERNAME);
 
         testStateChange(initialState, action, {
@@ -412,10 +406,10 @@ describe('password reset logic', () => {
     });
 
     it('should set mail error when a bad email has been set on verification', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_REQUEST,
             email: '!notAMail',
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.USERNAME);
 
         testStateChange(initialState, action, {
@@ -424,9 +418,9 @@ describe('password reset logic', () => {
     });
 
     it('should transition back to password reset OTP input and show error', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerFailure('thereHasBeenAnError');
 
         testStateChange(initialState, action, {
@@ -436,9 +430,9 @@ describe('password reset logic', () => {
     });
 
     it('should transition to login after password has been reset successfully', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerSuccess();
 
         testStateChange(initialState, action, {
@@ -447,9 +441,9 @@ describe('password reset logic', () => {
     });
 
     it('should go back to OTP input when password has not been reset successfully', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerFailure('thisCouldNotGoWorseThanThat');
 
         testStateChange(initialState, action, {
@@ -459,10 +453,9 @@ describe('password reset logic', () => {
     });
 
     it('should advance to OTP request', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_REQUEST_LOADING,
-        });
-
+        };
         const action = loginModalRemoteAnswerSuccess();
 
         testStateChange(initialState, action, {
@@ -471,10 +464,9 @@ describe('password reset logic', () => {
     });
 
     it('should fallback to reset request form', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_RESET_REQUEST_LOADING,
-        });
-
+        };
         const action = loginModalRemoteAnswerFailure('couldNotRequest');
 
         testStateChange(initialState, action, {
@@ -486,10 +478,10 @@ describe('password reset logic', () => {
 
 describe('OTP code resend logic', () => {
     it('should reset the error when OTP is sent again', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_OTP,
             error: new Error('thisShouldDisappear'),
-        });
+        };
         const action = loginModalRemoteRequest(LoginModalCheck.NONE);
 
         testStateChange(initialState, action, {
@@ -499,9 +491,9 @@ describe('OTP code resend logic', () => {
     });
 
     it('should move to OTP input after OTP has been resent', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_OTP_RESEND_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerSuccess();
 
         testStateChange(initialState, action, {
@@ -510,9 +502,9 @@ describe('OTP code resend logic', () => {
     });
 
     it('should move to OTP input after OTP could not have been resent', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.PASSWORD_CREATION_OTP_RESEND_LOADING,
-        });
+        };
         const action = loginModalRemoteAnswerFailure('couldNotResendOTP');
 
         testStateChange(initialState, action, {
@@ -524,14 +516,14 @@ describe('OTP code resend logic', () => {
 
 describe('logout logic', () => {
     it('should reset the whole state on user button click', () => {
-        const initialState = buildAuthenticationState({
+        const initialState = {
             step: AuthenticationStep.LOGGED_IN,
             email: 'reset@me.com',
             error: new Error('meToo!'),
             password: 'andMe',
             passwordRepeat: 'thisAsWell',
             OTP: '457674',
-        });
+        };
         const action = loginModalButtonClick(LoginModalButton.USER_BUTTON);
 
         testStateChange(initialState, action, initialAuthenticationState);
