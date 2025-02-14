e7ef97559c0db268550b7a02ff4a85e69d6d2f9f
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-14 19:59:42 +0600

    fix: - add device button send device creation request

--- src/components/website/mapApp/DeviceLocation/AddDeviceButton.tsx
+++ src/components/website/mapApp/DeviceLocation/AddDeviceButton.tsx
@@ -1,14 +1,14 @@
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 import { useAppDispatch } from '../../../../redux/store';
-import { deviceListRequest } from '../redux/DeviceAction';
+import { deviceCreateRequest } from '../redux/DeviceAction';
 
 export function AddDeviceButton() {
     const { t } = useI18next();
     const dispatch = useAppDispatch();
 
     return (
-        <button data-testid="createDeviceButton" onClick={() => dispatch(deviceListRequest())}>
+        <button data-testid="createDeviceButton" onClick={() => dispatch(deviceCreateRequest())}>
             {t('deviceAddDevice')}
         </button>
     );
