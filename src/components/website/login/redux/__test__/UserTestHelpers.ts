fce1bd14fdebe21753e7292ede5579be4b53555b
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-14 12:51:01 +0600

    test: - simplify tester by using generalised state observable

--- src/components/website/login/redux/__test__/UserTestHelpers.ts
+++ src/components/website/login/redux/__test__/UserTestHelpers.ts
@@ -1,16 +1,14 @@
 import { lastValueFrom, of, toArray } from 'rxjs';
 import { UsersClient } from '../../../../../redux/store';
 import { MapAppAction } from '../../../mapApp/redux/MapAppAction';
-import { buildMapAppState } from '../../../mapApp/redux/MapAppState';
 import { user } from '../User';
-import { buildStateForDevicesTest } from '../../../../../redux/__mocks__/stateBuilders';
+import { buildStateForCognitoTest } from '../../../../../redux/__mocks__/stateBuilders';
 
 export const userResolvingClient = () => Promise.resolve({ id: 'testUserId', points: 320 });
 export const userRejectingClient = () => Promise.reject('could not get logged in user data');
 
 export async function testUserEpic(userClient: UsersClient, action: MapAppAction, expectedAction: MapAppAction) {
-    const initialState = buildMapAppState({});
-    const output$ = user(of(action), buildStateForDevicesTest(initialState), {
+    const output$ = user(of(action), buildStateForCognitoTest({}), {
         usersClient: userClient,
     });
 
