7d0a64eacd4e5ba7cfc7632f88a0c0eaafddb638
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-17 11:42:37 +0600

    test: - use the new tester framework

--- src/components/website/mapApp/redux/__test__/UserTestHelpers.ts
+++ src/components/website/mapApp/redux/__test__/UserTestHelpers.ts
@@ -1,17 +1,12 @@
-import { lastValueFrom, of, toArray } from 'rxjs';
 import { UsersClient } from '../../../../../redux/store';
 import { MapAppAction } from '../MapAppAction';
 import { user } from '../User';
-import { buildTestStateObservable } from '../../../../../redux/__mocks__/state';
+import { buildEpicTester } from './devicesTestHelpers';
 
 export const userResolvingClient: UsersClient = { getUser: () => Promise.resolve({ id: 'testUserId', points: 320 }) };
 export const userRejectingClient: UsersClient = { getUser: () => Promise.reject('could not get logged in user data') };
 
 export async function testUserEpic(userClient: UsersClient, action: MapAppAction, expectedAction: MapAppAction) {
-    const output$ = user(of(action), buildTestStateObservable({}), {
-        usersClient: userClient,
-    });
-
-    const receivedAction = await lastValueFrom(output$.pipe(toArray()));
-    expect(receivedAction).toEqual([expectedAction]);
+    const testUserEpic = buildEpicTester(user);
+    await testUserEpic({ usersClient: userClient }, {}, action, [expectedAction]);
 }
