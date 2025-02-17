ac269c8b7ac2c226e790b2e67af17d360bfe2351
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-17 11:43:15 +0600

    test: . inline old tester

--- src/components/website/mapApp/redux/__test__/User.test.ts
+++ src/components/website/mapApp/redux/__test__/User.test.ts
@@ -1,18 +1,22 @@
 import { mapAppAuthenticationCompleted, mapAppGetLoggedInUserError, mapAppSetLoggedInUser } from '../MapAppAction';
-import { testUserEpic, userRejectingClient, userResolvingClient } from './UserTestHelpers';
+import { user } from '../User';
+import { userRejectingClient, userResolvingClient } from './UserTestHelpers';
+import { buildEpicTester } from './devicesTestHelpers';
 
 describe('user epic tests', () => {
     it('should get user points', async () => {
         const action = mapAppAuthenticationCompleted('testUserId');
         const expectedAction = mapAppSetLoggedInUser({ id: 'testUserId', points: 320 });
 
-        await testUserEpic(userResolvingClient, action, expectedAction);
+        const testUserEpic = buildEpicTester(user);
+        await testUserEpic({ usersClient: userResolvingClient }, {}, action, [expectedAction]);
     });
 
     it('should report remote error', async () => {
         const action = mapAppAuthenticationCompleted('testUserId');
         const expectedAction = mapAppGetLoggedInUserError('could not get logged in user data');
 
-        await testUserEpic(userRejectingClient, action, expectedAction);
+        const testUserEpic = buildEpicTester(user);
+        await testUserEpic({ usersClient: userRejectingClient }, {}, action, [expectedAction]);
     });
 });
--- src/components/website/mapApp/redux/__test__/UserTestHelpers.ts
+++ src/components/website/mapApp/redux/__test__/UserTestHelpers.ts
@@ -1,12 +1,4 @@
 import { UsersClient } from '../../../../../redux/store';
-import { MapAppAction } from '../MapAppAction';
-import { user } from '../User';
-import { buildEpicTester } from './devicesTestHelpers';
 
 export const userResolvingClient: UsersClient = { getUser: () => Promise.resolve({ id: 'testUserId', points: 320 }) };
 export const userRejectingClient: UsersClient = { getUser: () => Promise.reject('could not get logged in user data') };
-
-export async function testUserEpic(userClient: UsersClient, action: MapAppAction, expectedAction: MapAppAction) {
-    const testUserEpic = buildEpicTester(user);
-    await testUserEpic({ usersClient: userClient }, {}, action, [expectedAction]);
-}
