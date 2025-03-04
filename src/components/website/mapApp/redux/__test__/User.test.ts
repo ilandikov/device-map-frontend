9ec2dc04f6983ce702fbdd0eb73b733994328fd4
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-04 08:28:28 +0600

    test: - reuse tester

--- src/components/website/mapApp/redux/__test__/User.test.ts
+++ src/components/website/mapApp/redux/__test__/User.test.ts
@@ -1,6 +1,6 @@
 import { mapAppAuthenticationCompleted, mapAppGetLoggedInUserError, mapAppSetLoggedInUser } from '../MapAppAction';
 import { user } from '../User';
-import { itShouldAnswerBy, testEpicAnswerToAction } from '../../../../../redux/__test__/helpers';
+import { itShouldAnswerBy } from '../../../../../redux/__test__/helpers';
 import { userRejectingClient, userResolvingClient } from './UserTestHelpers';
 
 describe('user epic tests', () => {
@@ -12,13 +12,11 @@ describe('user epic tests', () => {
         expectedActions: [mapAppSetLoggedInUser({ id: 'testUserId', points: 320 })],
     });
 
-    it('should report remote error', async () => {
-        await testEpicAnswerToAction({
-            epic: user,
-            remoteClients: { usersClient: userRejectingClient },
-            partialRootState: {},
-            sentAction: mapAppAuthenticationCompleted('testUserId'),
-            expectedActions: [mapAppGetLoggedInUserError('could not get logged in user data')],
-        });
+    itShouldAnswerBy('sending an error about a user login going wrong', {
+        epic: user,
+        remoteClients: { usersClient: userRejectingClient },
+        partialRootState: {},
+        sentAction: mapAppAuthenticationCompleted('testUserId'),
+        expectedActions: [mapAppGetLoggedInUserError('could not get logged in user data')],
     });
 });
