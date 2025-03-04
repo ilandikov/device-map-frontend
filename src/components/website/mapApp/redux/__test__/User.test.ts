768823b60b1773ae7cda75d10f8ef701e9e0c727
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-04 07:51:25 +0600

    test: . inline variable

--- src/components/website/mapApp/redux/__test__/User.test.ts
+++ src/components/website/mapApp/redux/__test__/User.test.ts
@@ -3,20 +3,18 @@ import { user } from '../User';
 import { buildEpicTester } from '../../../../../redux/__test__/helpers';
 import { userRejectingClient, userResolvingClient } from './UserTestHelpers';
 
-const testUserEpic = buildEpicTester(user);
-
 describe('user epic tests', () => {
     it('should get user points', async () => {
         const action = mapAppAuthenticationCompleted('testUserId');
         const expectedAction = mapAppSetLoggedInUser({ id: 'testUserId', points: 320 });
 
-        await testUserEpic({ usersClient: userResolvingClient }, {}, action, [expectedAction]);
+        await buildEpicTester(user)({ usersClient: userResolvingClient }, {}, action, [expectedAction]);
     });
 
     it('should report remote error', async () => {
         const action = mapAppAuthenticationCompleted('testUserId');
         const expectedAction = mapAppGetLoggedInUserError('could not get logged in user data');
 
-        await testUserEpic({ usersClient: userRejectingClient }, {}, action, [expectedAction]);
+        await buildEpicTester(user)({ usersClient: userRejectingClient }, {}, action, [expectedAction]);
     });
 });
