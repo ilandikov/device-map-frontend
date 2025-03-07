f9ff1e082d235bf4fca7be0c7f514d961b322ac1
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-08 02:27:38 +0600

    test: - add device in test case to show change of behaviour

--- src/components/website/mapApp/DeviceLocation/__test__/DeviceLocation.test.tsx
+++ src/components/website/mapApp/DeviceLocation/__test__/DeviceLocation.test.tsx
@@ -54,6 +54,14 @@ describe('device list snapshot tests', () => {
                     location: { lat: 26.3553423, lon: 19.23131 },
                     approvals: 0,
                 },
+                {
+                    id: 'Matching selected marker and waiting for creation',
+                    createdDate: '0000000000001',
+                    lastUpdate: '0000000000002',
+                    creatorID: 'I created the third one!',
+                    location: { lat: 26.3553423, lon: 19.23131 },
+                    approvals: -1,
+                },
             ],
             selectedMarker: {
                 location: { lat: 26.3553423, lon: 19.23131 },
