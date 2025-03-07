6adf06801c012c087e1ddb9326851bb3d9db8d08
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-08 02:31:42 +0600

    refactor: - remove buttons from new device

--- src/components/website/mapApp/DeviceLocation/DeviceItemWaitingCreation.tsx
+++ src/components/website/mapApp/DeviceLocation/DeviceItemWaitingCreation.tsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import { T22Device } from '@mancho-school-t22/graphql-types';
 import { useI18next } from 'gatsby-plugin-react-i18next';
-import { useMapAppState } from '../redux/MapAppState';
 import { getDeviceItemType } from './DeviceItemType';
 import { DeviceItemContainer } from './DeviceItemContainer';
-import { DeleteButton } from './DeleteButton';
-import { ApproveButton } from './ApproveButton';
 
 export function DeviceItemWaitingCreation(props: { device: T22Device }) {
     const { t } = useI18next();
 
     const { device } = props;
-
-    const { loggedInUser } = useMapAppState();
-
-    const isDeviceCreatedByCurrentUser = device.creatorID === loggedInUser?.id;
-    const canBeDeleted = isDeviceCreatedByCurrentUser;
     const deviceItemType = getDeviceItemType(device.approvals ?? 0);
-    const canReceiveApprovals = deviceItemType === 'created' || deviceItemType === 'approving';
-    const canBeApproved = canReceiveApprovals && loggedInUser && !isDeviceCreatedByCurrentUser;
 
     return (
         <DeviceItemContainer deviceItemType={deviceItemType}>
             <p>{device.id}</p>
             <button className="device-list-item-opaque-text">{t('deviceReportBroken')}</button>
-            {canBeDeleted && <DeleteButton id={device.id} />}
-            {canBeApproved && <ApproveButton id={device.id} />}
         </DeviceItemContainer>
     );
 }
