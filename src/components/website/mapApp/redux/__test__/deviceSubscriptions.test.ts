2b16d1339b70d21670369d5a4d6784854b62ad9b
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-09 01:24:50 +0600

    test: - rewrite tests using new it()

--- src/components/website/mapApp/redux/__test__/deviceSubscriptions.test.ts
+++ src/components/website/mapApp/redux/__test__/deviceSubscriptions.test.ts
@@ -1,43 +1,29 @@
 import { deviceCreationSubscriptionRequest, deviceRemoteError, updateDevice } from '../DeviceAction';
-import { buildEpicTester } from '../../../../../redux/__test__/helpers';
+import { itShouldAnswerBy } from '../../../../../redux/__test__/helpers';
 import { deviceSubscriptions } from '../deviceSubscriptions';
 import { rejectingDeviceSubscriptionClient, resolvingDeviceSubscriptionClient } from './deviceSubscriptionHelpers';
 
-const testDeviceSubscriptionsEpic = buildEpicTester(deviceSubscriptions);
-
 describe('device subscription - creation', () => {
-    it('should receive new device after ', async () => {
-        const sentAction = deviceCreationSubscriptionRequest('id-to-be-created');
-        const expectedAction = updateDevice({
-            id: 'id-to-be-created',
-            creatorID: 'created-from-subscription',
-            createdDate: 12345678000,
-            lastUpdate: 23456789000,
-            location: { lat: 9, lon: 5 },
-            approvals: 4,
-        });
-
-        await testDeviceSubscriptionsEpic(
-            {
-                deviceSubscriptionClient: resolvingDeviceSubscriptionClient,
-            },
-            {},
-            sentAction,
-            [expectedAction],
-        );
+    itShouldAnswerBy('updating device', {
+        epic: deviceSubscriptions,
+        remoteClients: { deviceSubscriptionClient: resolvingDeviceSubscriptionClient },
+        sentAction: deviceCreationSubscriptionRequest('id-to-be-created'),
+        expectedActions: [
+            updateDevice({
+                id: 'id-to-be-created',
+                creatorID: 'created-from-subscription',
+                createdDate: 12345678000,
+                lastUpdate: 23456789000,
+                location: { lat: 9, lon: 5 },
+                approvals: 4,
+            }),
+        ],
     });
 
-    it('should notify about the error', async () => {
-        const sentAction = deviceCreationSubscriptionRequest('id-to-be-created');
-        const expectedAction = deviceRemoteError('could not subscribe to device update');
-
-        await testDeviceSubscriptionsEpic(
-            {
-                deviceSubscriptionClient: rejectingDeviceSubscriptionClient,
-            },
-            {},
-            sentAction,
-            [expectedAction],
-        );
+    itShouldAnswerBy('updating device', {
+        epic: deviceSubscriptions,
+        remoteClients: { deviceSubscriptionClient: rejectingDeviceSubscriptionClient },
+        sentAction: deviceCreationSubscriptionRequest('id-to-be-created'),
+        expectedActions: [deviceRemoteError('could not subscribe to device update')],
     });
 });
