08d79ccebac66fe45c9c77c37d9d862267bd437c
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-09 00:29:15 +0600

    refactor: - remove useless getDeviceItemType() call

--- src/components/website/mapApp/DeviceLocation/DeviceItemWaitingCreation.tsx
+++ src/components/website/mapApp/DeviceLocation/DeviceItemWaitingCreation.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { T22Device } from '@mancho-school-t22/graphql-types';
 import { useI18next } from 'gatsby-plugin-react-i18next';
-import { getDeviceItemType } from './DeviceItemType';
 import { DeviceItemContainer } from './DeviceItemContainer';
 
 export function DeviceItemWaitingCreation(props: { device: T22Device }) {
     const { t } = useI18next();
 
     const { device } = props;
-    const deviceItemType = getDeviceItemType(device.approvals ?? 0);
 
     return (
-        <DeviceItemContainer deviceItemType={deviceItemType}>
+        <DeviceItemContainer deviceItemType={'create'}>
             <p>{device.id}</p>
             <p>{t('deviceWaitingForCreation')}</p>
         </DeviceItemContainer>
