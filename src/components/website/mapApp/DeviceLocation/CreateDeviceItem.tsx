512539eb0a53e787ce35fb00d056714c57e1b8a1
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-11 18:52:56 +0600

    refactor: - inline component

--- src/components/website/mapApp/DeviceLocation/CreateDeviceItem.tsx
+++ src/components/website/mapApp/DeviceLocation/CreateDeviceItem.tsx
@@ -1,15 +1,25 @@
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import React from 'react';
+import { deviceCreateRequest, deviceCreationSubscriptionRequest } from '../redux/DeviceAction';
+import { useAppDispatch } from '../../../../redux/store';
 import { DeviceItemContainer } from './DeviceItemContainer';
-import { CreateDeviceButton } from './CreateDeviceButton';
 
 export function CreateDeviceItem() {
     const { t } = useI18next();
+    const dispatch = useAppDispatch();
 
     return (
         <DeviceItemContainer deviceItemType={'create'}>
             <p className="device-list-item-opaque-text">{t('deviceNoDeviceHere')}</p>
-            <CreateDeviceButton />
+            <button
+                data-testid="createDeviceButton"
+                onClick={() => {
+                    dispatch(deviceCreateRequest());
+                    dispatch(deviceCreationSubscriptionRequest());
+                }}
+            >
+                {t('deviceAddDevice')}
+            </button>
         </DeviceItemContainer>
     );
 }
