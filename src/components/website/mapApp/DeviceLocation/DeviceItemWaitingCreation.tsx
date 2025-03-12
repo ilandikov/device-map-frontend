9a259bcdf188f9bad8f32bbd320ce461ec60e40a
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-12 14:59:11 +0600

    feat: - do not show device id while waiting for device creation

--- src/components/website/mapApp/DeviceLocation/DeviceItemWaitingCreation.tsx
+++ src/components/website/mapApp/DeviceLocation/DeviceItemWaitingCreation.tsx
@@ -3,14 +3,11 @@ import { T22Device } from '@mancho-school-t22/graphql-types';
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import { DeviceItemContainer } from './DeviceItemContainer';
 
-export function DeviceItemWaitingCreation(props: { device: T22Device }) {
+export function DeviceItemWaitingCreation(_props: { device: T22Device }) {
     const { t } = useI18next();
-
-    const { device } = props;
 
     return (
         <DeviceItemContainer deviceItemType={'create'}>
-            <p>{device.id}</p>
             <p>{t('deviceWaitingForCreation')}</p>
         </DeviceItemContainer>
     );
