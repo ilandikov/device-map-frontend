cf5dc2ffdfb2dcb35d2137820c0d1164daa8d4df
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-12 15:00:46 +0600

    refactor: - remove unused prop

--- src/components/website/mapApp/DeviceLocation/DeviceItemWaitingCreation.tsx
+++ src/components/website/mapApp/DeviceLocation/DeviceItemWaitingCreation.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { T22Device } from '@mancho-school-t22/graphql-types';
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import { DeviceItemContainer } from './DeviceItemContainer';
 
-export function DeviceItemWaitingCreation(_props: { device: T22Device }) {
+export function DeviceItemWaitingCreation() {
     const { t } = useI18next();
 
     return (
