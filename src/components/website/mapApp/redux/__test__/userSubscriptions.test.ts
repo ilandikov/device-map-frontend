eb40415758175adf34e55f7b1c72eced86b6330f
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-14 18:39:18 +0600

    test: - remove unused client

--- src/components/website/mapApp/redux/__test__/userSubscriptions.test.ts
+++ src/components/website/mapApp/redux/__test__/userSubscriptions.test.ts
@@ -5,17 +5,13 @@ import {
     mapAppUpdateLoggedInUser,
     mapAppUserUpdateSubscriptionRequest,
 } from '../MapAppAction';
-import { rejectingDeviceSubscriptionClient, resolvingDeviceSubscriptionClient } from './deviceSubscriptionHelpers';
 import { userRejectingClient, userResolvingClient } from './UserTestHelpers';
 
 describe('user subscription', () => {
     itShouldAnswerBy('providing user data', {
         epic: userSubscriptions,
         partialRootState: { mapAppState: { loggedInUser: { id: 'i am waiting to receive points', points: 0 } } },
-        remoteClients: {
-            deviceSubscriptionClient: resolvingDeviceSubscriptionClient,
-            usersClient: userResolvingClient,
-        },
+        remoteClients: { usersClient: userResolvingClient },
         sentAction: mapAppUserUpdateSubscriptionRequest(),
         expectedActions: [mapAppUpdateLoggedInUser({ id: 'i am waiting to receive points', points: 100 })],
     });
@@ -23,10 +19,7 @@ describe('user subscription', () => {
     itShouldAnswerBy('sending error', {
         epic: userSubscriptions,
         partialRootState: { mapAppState: { loggedInUser: { id: 'i will create subscription', points: 0 } } },
-        remoteClients: {
-            deviceSubscriptionClient: rejectingDeviceSubscriptionClient,
-            usersClient: userRejectingClient,
-        },
+        remoteClients: { usersClient: userRejectingClient },
         sentAction: mapAppUserUpdateSubscriptionRequest(),
         expectedActions: [mapAppSubscriptionError('could not subscribe to user update')],
     });
