1e1e423ea0070fea798553908dd5d9681d8f0803
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-14 18:34:04 +0600

    test: - remove unused client

--- src/components/website/mapApp/redux/__test__/deviceSubscriptions.test.ts
+++ src/components/website/mapApp/redux/__test__/deviceSubscriptions.test.ts
@@ -1,17 +1,13 @@
 import { deviceCreated, deviceCreation, deviceCreationSubscriptionRequest, deviceRemoteError } from '../DeviceAction';
 import { itShouldAnswerBy } from '../../../../../redux/__test__/helpers';
 import { deviceSubscriptions } from '../deviceSubscriptions';
-import { rejectingDeviceSubscriptionClient, resolvingDeviceSubscriptionClient } from './deviceSubscriptionHelpers';
 import { rejectingDevicesClient, resolvingDevicesClient } from './devicesTestHelpers';
 
 describe('device subscription - creation', () => {
     itShouldAnswerBy('updating device', {
         epic: deviceSubscriptions,
         partialRootState: { mapAppState: { loggedInUser: { id: 'i will create subscription', points: 0 } } },
-        remoteClients: {
-            deviceSubscriptionClient: resolvingDeviceSubscriptionClient,
-            devicesClient: resolvingDevicesClient,
-        },
+        remoteClients: { devicesClient: resolvingDevicesClient },
         sentAction: deviceCreationSubscriptionRequest(),
         expectedActions: [
             deviceCreated({
@@ -29,10 +25,7 @@ describe('device subscription - creation', () => {
     itShouldAnswerBy('updating device', {
         epic: deviceSubscriptions,
         partialRootState: { mapAppState: { loggedInUser: { id: 'i will create subscription', points: 0 } } },
-        remoteClients: {
-            deviceSubscriptionClient: rejectingDeviceSubscriptionClient,
-            devicesClient: rejectingDevicesClient,
-        },
+        remoteClients: { devicesClient: rejectingDevicesClient },
         sentAction: deviceCreationSubscriptionRequest(),
         expectedActions: [deviceRemoteError('could not subscribe to device update'), deviceCreation(false)],
     });
