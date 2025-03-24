72675917c96da50155480b9e10e48c8bf4d35b8e
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-24 14:17:17 +0600

    refactor: ! filter action by sub type

--- src/components/website/login/redux/userSubscriptionSender.ts
+++ src/components/website/login/redux/userSubscriptionSender.ts
@@ -1,14 +1,19 @@
 import { ofType } from 'redux-observable';
-import { EMPTY, catchError, map, switchMap } from 'rxjs';
+import { EMPTY, catchError, filter, map, switchMap } from 'rxjs';
 import { fromPromise } from 'rxjs/internal/observable/innerFrom';
 import { RootEpic } from '../../../../redux/store';
 import { MapAppActionType } from '../../mapApp/redux/MapAppAction';
 import { setAuthenticatedClient } from '../../../../client/graphql';
-import { loggedInUserSubscriptionRequest } from '../../mapApp/redux/LoggedInUserAction';
+import {
+    LoggedInUserAction,
+    LoggedInUserActionType,
+    loggedInUserSubscriptionRequest,
+} from '../../mapApp/redux/LoggedInUserAction';
 
 export const userSubscriptionSender: RootEpic = (action$) => {
     return action$.pipe(
         ofType(MapAppActionType.LOGGED_IN_USER_SET_ID),
+        filter((action: LoggedInUserAction) => action.subType === LoggedInUserActionType.SET_ID),
         switchMap(() =>
             fromPromise(setAuthenticatedClient()).pipe(
                 map(() => loggedInUserSubscriptionRequest()),
