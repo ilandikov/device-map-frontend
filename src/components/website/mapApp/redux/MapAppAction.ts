c3dcecb2b062512cbcbda802e2ced51450ed4409
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-03-24 14:19:38 +0600

    refactor: - remove unused action types

--- src/components/website/mapApp/redux/MapAppAction.ts
+++ src/components/website/mapApp/redux/MapAppAction.ts
@@ -5,15 +5,9 @@ import { SelectedMarkerAction } from './SelectedMarkerAction';
 
 export enum MapAppActionType {
     LOGIN_MODAL_CLOSE = 'LOGIN_MODAL_CLOSE',
-    LOGGED_IN_USER_RESET = 'LOGGED_IN_USER_RESET',
-    LOGGED_IN_USER_SET_ID = 'LOGGED_IN_USER_SET_ID',
     SELECTED_MARKER = 'SELECTED_MARKER',
-    LOGGED_IN_USER_SET = 'LOGGED_IN_USER_SET',
-    LOGGED_IN_USER_ERROR = 'LOGGED_IN_USER_ERROR',
-    LOGGED_IN_USER_UPDATE = 'LOGGED_IN_USER_UPDATE',
     SHOW_COMPONENT = 'SHOW_COMPONENT',
     LOGGED_IN_USER = 'LOGGED_IN_USER',
-    LOGGED_IN_USER_SUBSCRIPTION_ERROR = 'LOGGED_IN_USER_SUBSCRIPTION_ERROR',
 }
 
 export type MapAppAction = MapAppShowComponent | LoggedInUserAction | SelectedMarkerAction | DeviceAction;
